3f28de87c6dc63c109ceb4e9336476e90df693c4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-02-12 15:39:07 +0000

    * Generic configuration for build machines.
    svn path=/configurations/trunk/tud/; revision=7897

---
+++ build-machines.nix
@@ -0,0 +1,35 @@
+{
+  boot = {
+    grubDevice = "/dev/sda";
+    initrd = {
+      extraKernelModules = ["3w_xxxx"];
+    };
+  };
+
+  fileSystems = [
+    { mountPoint = "/";
+      label = "nixos";
+    }
+  ];
+
+  swapDevices = [
+    { label = "swap"; }
+  ];
+
+  networking = {
+    useDHCP = false;
+    interfaces = {
+      interface = "eth0";
+      ipAddress = "192.168.1.14";
+    };
+    defaultGateway = "192.168.1.1";
+    nameservers = ["130.161.158.4" "130.161.33.17" "130.161.180.1"];
+  };
+  
+  services = {
+    sshd = {
+      enable = true;
+    };
+  };
+
+}
