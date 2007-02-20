f36e509e835dc9c7bc64db31e200300a2da5527e
Martin Bravenboer <mbravenboer@unknown>
2007-02-20 15:13:31 +0000

    Basics of dist-manager. Not working yet, need to make the dist manager service more configurable.
    svn path=/configurations/trunk/tud/; revision=7927

--- main.nix
+++ main.nix
@@ -1,9 +1,10 @@
-{
+rec {
   boot = {
     grubDevice = "/dev/sda";
     initrd = {
       extraKernelModules = ["arcmsr"];
     };
+
     extraKernelModules = ["kvm-intel"];
   };
 
@@ -36,22 +37,36 @@
       hostName = "dutiel.st.ewi.tudelft.nl";
 
       subservices = {
-
         subversion = {
           enable = true;
           dataDir = "/data/subversion";
           notificationSender = "svn@example.org";
           userCreationDomain = "st.ewi.tudelft.nl";
         };
-
       };
 
-      extraSubservices = [
-        
-      ];
-    
+      extraSubservices = {
+        enable = true;
+        services = webServer : pkgs : [
+          (distManagerService webServer pkgs)        
+        ];
+      };
     };
-
   };
 
+  distManagerService = webServer : pkgs :
+    (distManager webServer pkgs) {
+      distDir = "/data/webserver/dist";
+      distPrefix = "/dist";
+      distConfDir = "/data/webserver/dist-conf";
+    };
+
+  distManager = webServer : pkgs : { distDir, distPrefix, distConfDir } :
+    import ../../services/dist-manager {
+      inherit (pkgs) stdenv perl;
+      saxon8 = pkgs.saxonb;
+      inherit distDir distPrefix distConfDir;
+      canonicalName = "http://" + webServer.hostName + 
+        (if webServer.httpPort == 80 then "" else ":" + (toString webServer.httpPort));
+    };
 }
