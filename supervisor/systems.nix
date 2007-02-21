6f340dda23bc4987c9443beeb88c87ee95e49073
Martin Bravenboer <mbravenboer@unknown>
2007-02-21 16:52:46 +0000

    Systems description
    svn path=/configurations/trunk/tud/; revision=7975

---
+++ supervisor/systems.nix
@@ -0,0 +1,10 @@
+[ { system = "i686-linux"; }
+  { system = "x86_64-linux"; }
+  { system = "i686-cygwin"; }
+  { system = "i686-cygwin"; stdenvType = "i686-mingw"; }
+  { system = "i686-darwin"; }
+
+  # { system = "powerpc-darwin"; }
+  # { system = "powerpc-linux"; }
+  # {system = "i686-freebsd";}
+]
