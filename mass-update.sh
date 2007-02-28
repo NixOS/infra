9ce25471d0500a97f54a419094c7c0022de44290
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-02-28 13:21:58 +0000

    svn path=/configurations/trunk/tud/; revision=8098

--- mass-update.sh
+++ mass-update.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
+export SSH_AUTH_SOCK= # hack
+
 for i in 127.0.0.1 192.168.1.13 192.168.1.14 "127.0.0.1 -p 2222"; do
     echo "=== Updating $i ==="
-    ssh -i /root/.ssh/id_mass_update root@$i sh --login -c '"cd /etc/nixos && svn up nixos nixpkgs configurations releases && nix-channel --update && nixos-rebuild switch"'
+    ssh -i /root/.ssh/id_mass_update root@$i sh --login -c '"cd /etc/nixos && svn up nixos nixpkgs configurations release && nix-channel --update && nixos-rebuild switch"'
 done
