30afae832706638680d6b266dc29c6ca4b47cf2e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2007-03-16 13:32:43 +0000

    * Use DHCP to get the network configuration for the build machines.
    svn path=/configurations/trunk/tud/; revision=8301

--- build-machines.nix
+++ build-machines.nix
@@ -20,17 +20,6 @@
     maxJobs = 2;
   };
   
-  networking = {
-    useDHCP = false;
-    interfaces = [
-      { name = "eth0";
-        ipAddress = "192.168.1.14";
-      }
-    ];
-    defaultGateway = "192.168.1.5";
-    nameservers = ["130.161.158.4" "130.161.33.17" "130.161.180.1"];
-  };
-  
   services = {
     sshd = {
       enable = true;
