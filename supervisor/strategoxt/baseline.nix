39bd7cb87d92643dfd6f77459e64ff87b98ffb09
Martin Bravenboer <mbravenboer@unknown>
2008-03-28 18:59:10 +0000

    svn path=/configurations/trunk/tud/; revision=11329

---
+++ supervisor/strategoxt/baseline.nix
@@ -0,0 +1,6 @@
+{
+  aterm = http://buildfarm.st.ewi.tudelft.nl/releases/meta-environment/aterm-2.5pre21238-l2q7rg38/;
+  sdf = http://buildfarm.st.ewi.tudelft.nl/releases/meta-environment/sdf2-bundle-2.4pre212034-2nspl1xc/;
+  strategoxt = http://buildfarm.st.ewi.tudelft.nl/releases/strategoxt/strategoxt-0.17M3pre17483/;
+  strategoLibraries = http://buildfarm.st.ewi.tudelft.nl/releases/strategoxt/stratego-libraries-0.17pre17483-zvwcks5g/;
+}
