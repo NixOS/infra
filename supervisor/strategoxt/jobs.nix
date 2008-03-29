e1cfffd8e38433143a58146ac86c7e8096ca0a18
Martin Bravenboer <mbravenboer@unknown>
2008-03-29 23:31:46 +0000

    revert to the compact job description
    svn path=/configurations/trunk/tud/; revision=11346

--- supervisor/strategoxt/jobs.nix
+++ supervisor/strategoxt/jobs.nix
@@ -4,21 +4,24 @@ let easy = (import ./easy-job.nix) attrs;
     makeEasyJob = easy.makeEasyJob;
     makeStrategoXTJob = easy.makeStrategoXTJob;
 
-#    specs =
-#      (import ../../../../release/jobs/strategoxt2/packages.nix);
+    specs =
+      (import ../../../../release/jobs/strategoxt2/packages.nix);
 
  in {
-  /*
+
   javaFrontSyntaxTrunk = makeEasyJob {
     spec = specs.javaFrontSyntax;
     stable = false;
     svn = "trunk";
-  }; */
+  };
+}
 
+
+/*
   javaFrontSyntaxTrunk2 = makeStrategoXTJob {
     dirName = "java-front-syntax2";
     inputs = {
-      javaFrontSyntax = attrs.svnInput https://svn.cs.uu.nl:12443/repos/StrategoXT/java-front/trunk/syntax;
+      javaFrontSyntaxCheckout = attrs.svnInput https://svn.cs.uu.nl:12443/repos/StrategoXT/java-front/trunk/syntax;
       atermInfo = easy.baseline.aterm;
       sdf2BundleInfo = easy.baseline.sdf;
       strategoxtInfo = easy.baseline.strategoxt;
@@ -27,22 +30,4 @@ let easy = (import ./easy-job.nix) attrs;
     notifyAddresses = ["martin.bravenboer@gmail.com"];
     jobAttr = "javaFrontSyntaxUnstable";
   };
-}
-
-/*
-
-{
-  javaFrontSyntaxTrunk = job {
-    dirName = "java-front-syntax";
-    inputs = {
-      javaFrontSyntax = attrs.svnInput https://svn.cs.uu.nl:12443/repos/StrategoXT/java-front/trunk/syntax;
-      atermInfo = baseline.aterm;
-      sdf2BundleInfo = baseline.sdf;
-      strategoxtInfo = baseline.strategoxt;
-    };
-
-    notifyAddresses = [bravenboer];
-    jobAttr = "javaFrontSyntaxUnstable";
-  };
-}
-  */
+*/
