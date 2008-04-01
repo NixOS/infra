44aaf4b5a3f6c3ed5ef0757f6efe859a7b5f91a0
Martin Bravenboer <mbravenboer@unknown>
2008-04-01 20:39:36 +0000

    Updated the baselines to releases at releases.strategoxt.org (except for sdf)
    svn path=/configurations/trunk/tud/; revision=11450

--- supervisor/strategoxt/baseline.nix
+++ supervisor/strategoxt/baseline.nix
@@ -1,19 +1,21 @@
 {
   aterm =
-    http://buildfarm.st.ewi.tudelft.nl/releases/meta-environment/aterm-2.5pre21238-l2q7rg38/;
+    http://releases.strategoxt.org/aterm64/aterm-2.6pre24721-wcjn9g8z/;
 
+  # martin: needs to be updated, but I'm scared what will happen. We
+  # should do this when the buildfarm transition has stabilized a bit.
   sdf =
     http://buildfarm.st.ewi.tudelft.nl/releases/meta-environment/sdf2-bundle-2.4pre212034-2nspl1xc/;
 
   strategoxt =
-    http://buildfarm.st.ewi.tudelft.nl/releases/strategoxt/strategoxt-0.17M3pre17632/;
+    http://releases.strategoxt.org/strategoxt/strategoxt-0.17M3pre17632-f440vxhd/;
 
   strategoLibraries =
-    http://buildfarm.st.ewi.tudelft.nl/releases/strategoxt/stratego-libraries-0.17pre17483-zvwcks5g/;
+    http://releases.strategoxt.org/stratego-libraries/stratego-libraries-0.17pre1763217632-vfcwm49q/;
 
   # used for compiling Stratego/XT from Subversion.
   strategoxtBootstrap =
-    http://buildfarm.st.ewi.tudelft.nl/releases/strategoxt/strategoxt-0.17M3pre17632/strategoxt-0.17M3pre17632.tar.gz;
+    http://releases.strategoxt.org/strategoxt/strategoxt-0.17M3pre17632-f440vxhd/strategoxt-0.17M3pre17632.tar.gz;
     # ftp server is down a lot at the moment
     # ftp://ftp.strategoxt.org/pub/stratego/StrategoXT/baseline/latest/strategoxt.tar.gz
 }
