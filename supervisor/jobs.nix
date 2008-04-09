f890e9aa93d6dd5add0462920ec09ac8e115631b
Martin Bravenboer <mbravenboer@unknown>
2008-04-09 22:14:19 +0000

    Bump nixpkgs baseline to 11515 to get RPM features
    svn path=/configurations/trunk/tud/; revision=11517

--- supervisor/jobs.nix
+++ supervisor/jobs.nix
@@ -11,7 +11,7 @@ let
     inputs = {
       job = pathInput ../../../release;
       #job = svnInput jobBaseline;
-      nixpkgs = svnInputRev nixpkgsBaseline 11301;
+      nixpkgs = svnInputRev nixpkgsBaseline 11515;
     } // attrs.inputs;
   };
 
