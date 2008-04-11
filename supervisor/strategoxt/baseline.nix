b429fecc297924daa0cb577473b519370071ee99
Martin Bravenboer <mbravenboer@unknown>
2008-04-11 22:14:13 +0000

    svn path=/configurations/trunk/tud/; revision=11596

--- supervisor/strategoxt/baseline.nix
+++ supervisor/strategoxt/baseline.nix
@@ -1,6 +1,6 @@
 {
   aterm =
-    http://releases.strategoxt.org/aterm64/aterm-2.5pre21238-bdw75bjj/;
+    http://releases.strategoxt.org/strategoxt-0.17/aterm/aterm-2.5pre21238-8vxvj9gm/;
 
   # martin: needs to be updated, but I'm scared what will happen. We
   # should do this when the buildfarm transition has stabilized a bit.
