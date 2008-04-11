ef41fb6d55bf2fdd16d53c53de44b535155730ef
Martin Bravenboer <mbravenboer@unknown>
2008-04-11 23:50:19 +0000

    svn path=/configurations/trunk/tud/; revision=11602

--- supervisor/strategoxt/baseline.nix
+++ supervisor/strategoxt/baseline.nix
@@ -5,7 +5,7 @@
   # martin: needs to be updated, but I'm scared what will happen. We
   # should do this when the buildfarm transition has stabilized a bit.
   sdf =
-    http://releases.strategoxt.org/strategoxt-0.17/sdf2-bundle/unstable/;
+    http://releases.strategoxt.org/strategoxt-0.17/sdf2-bundle/sdf2-bundle-2.4pre212034-37nm9z7p/;
 
   strategoxt =
     http://releases.strategoxt.org/strategoxt/strategoxt-0.17M3pre17664-r77nysmx/;
