d3d232e15188c07abd9beec92b11c1c1fc8bd13b
Martin Bravenboer <mbravenboer@unknown>
2008-04-14 23:47:21 +0000

    Updates of baselines
    svn path=/configurations/trunk/tud/; revision=11626

--- supervisor/strategoxt/baseline.nix
+++ supervisor/strategoxt/baseline.nix
@@ -8,14 +8,14 @@
     http://releases.strategoxt.org/strategoxt-0.17/sdf2-bundle/sdf2-bundle-2.4pre212034-37nm9z7p/;
 
   strategoxt =
-    http://releases.strategoxt.org/strategoxt/strategoxt-0.17M3pre17683-xcg4wc0j/;
+    http://releases.strategoxt.org/strategoxt/strategoxt-0.17M3pre17690-0ryq08iv/;
 
   strategoLibraries =
-    http://releases.strategoxt.org/stratego-libraries/stratego-libraries-0.17pre1766417664-cn5vri69/;
+    http://releases.strategoxt.org/stratego-libraries/stratego-libraries-0.17pre1768017680-hjqcxi9w/;
 
   # used for compiling Stratego/XT from Subversion.
   strategoxtBootstrap =
-    http://releases.strategoxt.org/strategoxt/strategoxt-0.17M3pre17683-xcg4wc0j/strategoxt-0.17M3pre17683.tar.gz;
+    http://releases.strategoxt.org/strategoxt/strategoxt-0.17M3pre17690-0ryq08iv/strategoxt-0.17M3pre17690.tar.gz;
     # ftp server is down a lot at the moment
     # ftp://ftp.strategoxt.org/pub/stratego/StrategoXT/baseline/latest/strategoxt.tar.gz
 }
