881b9faebf5a7103e2a3fb7da8f65928df4c9be8
Martin Bravenboer <mbravenboer@unknown>
2008-06-16 23:20:15 +0000

    Bump nixpkgs to 12130 to get MinGW fix
    svn path=/configurations/trunk/tud/; revision=12131

--- buildfarm/jobs.nix
+++ buildfarm/jobs.nix
@@ -11,7 +11,7 @@ let
     inputs = {
       job = pathInput ../../../release;
       #job = svnInput jobBaseline;
-      nixpkgs = svnInputRev nixpkgsBaseline 11922;
+      nixpkgs = svnInputRev nixpkgsBaseline 12130;
     } // attrs.inputs;
   };
 
