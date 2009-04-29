674d6c34c92344fbcc33b8294570f384093d7d20
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-04-29 15:48:22 +0000

    svn path=/configurations/trunk/tud/; revision=15399

---
+++ build-machines-1950.nix
@@ -0,0 +1,54 @@
+# Configuration for the Dell PowerEdge 1950 build machines.
+
+{pkgs, config, ...}:
+
+{
+  boot = {
+    grubDevice = "/dev/sda";
+    initrd = {
+      extraKernelModules = ["uhci_hcd" "ehci_hcd" "ata_piix" "mptsas" "usbhid" "ext4"];
+    };
+    kernelModules = ["acpi-cpufreq" "kvm-intel"];
+    kernelPackages = pkgs: pkgs.kernelPackages_2_6_28;
+    copyKernels = true;
+  };
+
+  fileSystems = [
+    { mountPoint = "/";
+      label = "nixos";
+      options = "noatime";
+    }
+  ];
+
+  swapDevices = [
+    { label = "swap"; }
+  ];
+
+  nix = {
+    maxJobs = 8;
+    extraOptions = ''
+      build-max-silent-time = 3600
+    '';
+  };
+  
+  services = {
+    sshd = {
+      enable = true;
+    };
+    /*
+    zabbixAgent = {
+      enable = true;
+      server = "192.168.1.5";
+    };
+    */
+    cron = {
+      systemCronJobs = [
+        "15 03 * * * root ${pkgs.nixUnstable}/bin/nix-collect-garbage --max-atime $(date +\\%s -d '2 weeks ago') > /var/log/gc.log 2>&1"
+      ];
+    };
+  };
+
+  networking = {
+    hostName = ""; # obtain from DHCP server
+  };
+}
