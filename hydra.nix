22cfb18a2d7497d1b04610f794f18219addf38e7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-07-03 09:20:19 +0000

    svn path=/configurations/trunk/tud/; revision=16156

--- hydra.nix
+++ hydra.nix
@@ -1,5 +1,20 @@
 {config, pkgs, ...}:
 
+let
+
+  machines = import ./machines.nix;
+
+  # Produce the list of Nix build machines in the format expected by
+  # the Nix daemon Upstart job.
+  buildMachines =
+    let addKey = machine: machine // 
+      { sshKey = "/root/.ssh/id_buildfarm";
+        sshUser = machine.buildUser;
+      };
+    in map addKey (pkgs.lib.filter (machine: machine ? buildUser) machines);
+
+in
+
 {
   boot = {
     initrd = {
@@ -8,11 +23,17 @@
     kernelModules = [ "acpi-cpufreq" "kvm-intel" ];
     kernelPackages = pkgs.kernelPackages_2_6_28;
     grubDevice = "/dev/sda";
+    copyKernels = true;
+  
+    localCommands = ''
+      echo 60 > /proc/sys/kernel/panic
+    '';
   };
 
   fileSystems = [
     { mountPoint = "/"; 
       label = "nixos";
+      options = "noatime";
     }
   ];
  
@@ -21,12 +42,31 @@
   ];
 
   nix = {
-    maxJobs = 8;
+    maxJobs = 0;
+    distributedBuilds = true;
+    inherit buildMachines;
+
+    extraOptions = ''
+      gc-keep-outputs = true
+
+      # The default (`true') slows Nix down a lot since the build farm
+      # has so many GC roots.
+      gc-check-reachability = false
+
+      # Hydra needs caching of build failures.
+      build-cache-failure = true
+
+      build-poll-interval = 10
+    '';
   };
 
   networking = {
     hostName = "hydra";
     domain = "buildfarm";
+
+    extraHosts = 
+      let toHosts = m: "${m.ipAddress} ${m.hostName} ${pkgs.lib.concatStringsSep " " (if m ? aliases then m.aliases else [])}\n"; in
+      pkgs.lib.concatStrings (map toHosts machines);
   };
 
   services = {
