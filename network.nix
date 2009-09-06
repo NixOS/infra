b11dde5b37c0f1616da1c3ec012777442074da7e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-09-06 16:05:44 +0000

    svn path=/configurations/trunk/tud/; revision=16970

--- network.nix
+++ network.nix
@@ -6,6 +6,6 @@
   kenny = import ./build-machines-dell-1950.nix;
   stan = import ./build-machines-dell-1950.nix;
   kyle = import ./build-machines-dell-1950.nix;
-  #cartman = import ./cartman.nix;
+  cartman = import ./cartman.nix;
   hydra = import ./hydra.nix;
 }
