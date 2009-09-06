4396c8544003ed23c38900f6714761b28cf9733b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-09-06 16:02:23 +0000

    * nixos-deploy-network configuration for the buildfarm.
    svn path=/configurations/trunk/tud/; revision=16969

---
+++ infrastructure.nix
@@ -0,0 +1,39 @@
+{
+  # !!! should be merged with machines.nix
+  terrance =
+    { hostName = "terrance";
+      system = "x86_64-linux";
+    };
+  phillip =
+    { hostName = "phillip";
+      system = "x86_64-linux";
+    };
+  jimmy =
+    { hostName = "jimmy";
+      system = "i686-linux";
+    };
+  timmy =
+    { hostName = "timmy";
+      system = "i686-linux";
+    };
+  kenny = 
+    { hostName = "kenny";
+      system = "x86_64-linux";
+    };
+  stan = 
+    { hostName = "stan";
+      system = "x86_64-linux";
+    };
+  kyle = 
+    { hostName = "kyle";
+      system = "x86_64-linux";
+    };
+  cartman = 
+    { hostName = "cartman";
+      system = "i686-linux";
+    };
+  hydra = 
+    { hostName = "hydra";
+      system = "x86_64-linux";
+    };
+}
---
+++ network.nix
@@ -0,0 +1,11 @@
+{
+  terrance = import ./build-machines-hps.nix;
+  phillip = import ./build-machines-hps.nix;
+  jimmy = import ./build-machines-hps.nix;
+  timmy = import ./build-machines-hps.nix;
+  kenny = import ./build-machines-dell-1950.nix;
+  stan = import ./build-machines-dell-1950.nix;
+  kyle = import ./build-machines-dell-1950.nix;
+  #cartman = import ./cartman.nix;
+  hydra = import ./hydra.nix;
+}
