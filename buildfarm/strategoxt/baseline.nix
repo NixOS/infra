df6982b5ff80a8f44302d059b39f2e082e9f0418
Lennart Kats <LennartKats@unknown>
2009-09-15 11:28:03 +0000

    New Stratego/XT baseline. Now using urls in the new buildfarm and reserving a couple of keywords, hopefully without breaking anything.
    svn path=/configurations/trunk/tud/; revision=17156

--- buildfarm/strategoxt/baseline.nix
+++ buildfarm/strategoxt/baseline.nix
@@ -8,14 +8,14 @@
     http://releases.strategoxt.org/strategoxt-0.17/sdf2-bundle/sdf2-bundle-2.4pre212034-37nm9z7p/;
 
   strategoxt =
-    http://releases.strategoxt.org/strategoxt/strategoxt-0.17pre18583-i573smdl/;
+    http://hydra.nixos.org/build/66385; # r19956
 
   strategoLibraries =
     http://releases.strategoxt.org/stratego-libraries/stratego-libraries-0.17pre1768017680-hjqcxi9w/;
 
   # used for compiling Stratego/XT from Subversion.
   strategoxtBootstrap =
-    http://releases.strategoxt.org/strategoxt/strategoxt-0.17pre18583-i573smdl/strategoxt-0.17pre18583.tar.gz;
+    http://hydra.nixos.org/build/66370/download/1/strategoxt-0.18pre19956.tar.gz; # r19956
 
   /*
     ftp://ftp.strategoxt.org/pub/stratego/StrategoXT/baseline/latest/strategoxt.tar.gz;
