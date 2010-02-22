bfe7b610f033dbdf864f6cb52f4abaf6f66e5865
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-02-22 16:21:04 +0000

    * Monitor the CPU temperature.
    svn path=/configurations/trunk/tud/; revision=20177

--- common.nix
+++ common.nix
@@ -21,4 +21,8 @@
 
   services.zabbixAgent.enable = true;
   services.zabbixAgent.server = "192.168.1.5,127.0.0.1";
+  services.zabbixAgent.extraConfig =
+    ''
+      UserParameter=hardware.temp.cpu.average,cat /sys/devices/platform/coretemp.*/temp1_input | ${pkgs.perl}/bin/perl -e 'while (<>) { $n++; $sum += $_; }; print $sum / $n / 1000;'
+    '';
 }
