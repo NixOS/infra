6915455f63236a288906e6419ea9c8e8a216b36d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2010-12-06 09:54:28 +0000

    svn path=/configurations/trunk/tud/; revision=24989

--- mrhankey.nix
+++ mrhankey.nix
@@ -1,5 +1,7 @@
 { config, pkgs, ... }:
 
+with pkgs.lib;
+
 {
   require = [ ./common.nix ];
 
@@ -21,4 +23,19 @@
   networking.hostName = "";
 
   services.openssh.enable = true;
+
+  environment.etc =
+    flip map (range 0 9) (nr:
+      { source = pkgs.writeText "agilecloud0${toString nr}"
+          ''
+            from xen.util.path import *
+            memory = 512
+            kernel = XENFIRMWAREDIR + '/pv-grub-x86_32.gz'
+            extra = '(hd0)/boot/grub/menu.lst'
+            disk = [ 'file:/vmdisks/agilecloud-0${toString nr}-root.img,xvda1,w' ]
+            vif = [ 'mac=00:16:3e:00:34:0${toString nr}' ]
+          '';
+        target = "xen/agilecloud0${toString nr}";
+      }
+    );
 }
