393516407cef74568b73d557253d90ec1778d7e3
Rob Vermaas <rob.vermaas@gmail.com>
2011-05-09 15:02:17 +0000

    leftover commit
    svn path=/configurations/trunk/tud/; revision=27199

--- lucifer.nix
+++ lucifer.nix
@@ -5,6 +5,8 @@
 
   nixpkgs.system = "x86_64-linux";
 
+  environment.systemPackages = [ pkgs.wget ];
+
   networking.hostName = "lucifer";
 
   boot.loader.grub.device = "/dev/sda";
@@ -13,6 +15,22 @@
   boot.kernelModules = [ "acpi-cpufreq" "kvm-intel" ];
 
   services.hydra.enable = true;
+  services.hydra.tracker = ''
+    <!-- Start of StatCounter Code -->
+    <script type=\"text/javascript\">
+      var sc_project=6818408;
+      var sc_invisible=1;
+      var sc_security=\"8838c8ed\";
+    </script>
+    <script type=\"text/javascript\"
+      src=\"http://www.statcounter.com/counter/counter.js\"></script>
+    <noscript><div class=\"statcounter\"><a title=\"visit tracker
+    on tumblr\" href=\"http://statcounter.com/tumblr/\"
+    target=\"_blank\"><img class=\"statcounter\"
+    src=\"http://c.statcounter.com/6818408/0/8838c8ed/1/\"
+    alt=\"visit tracker on tumblr\"></a></div></noscript>
+    <!-- End of StatCounter Code -->
+  '';
 
   fileSystems = 
     [ { mountPoint = "/";
@@ -68,5 +86,9 @@
       + "-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8999 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=localhost "
       + "-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/data/tomcat/logs/java_pid<pid>.hprof";
   };
+
+
+  services.cron.systemCronJobs =
+    [ "*/5 * * * *  hydra-mirror  flock -x /data/releases/.lock -c /home/hydra-mirror/release/mirror/mirror-nixos-isos.sh >> /home/hydra-mirror/nixos-mirror.log 2>&1" ];
 
 }
