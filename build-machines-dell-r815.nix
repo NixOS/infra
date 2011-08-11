366e45730e6f9cbb79014bffe0322e4ca322ed77
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-08-11 12:33:59 +0000

    svn path=/configurations/trunk/tud/; revision=28491

---
+++ build-machines-dell-r815.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  boot.loader.grub.version = 2;
+  require = [ ./build-machines-common.nix ];
+  nixpkgs.system = "x86_64-linux";
+
+  boot.initrd.kernelModules = [ "megaraid_sas" "ext4" ];
+  boot.kernelModules = [ "acpi-cpufreq" "kvm-amd" ];
+
+  nix.maxJobs = 48;
+
+  fileSystems = [ 
+    { device = "none"; fsType = "tmpfs"; mountPoint = "/tmp"; options = "size=16G"; neededForBoot=true; } 
+  ];
+}
