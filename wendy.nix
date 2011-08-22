bbde8cd750168f01b04c12bc144608a1180f3e87
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-08-22 16:26:43 +0000

    svn path=/configurations/trunk/tud/; revision=28741

---
+++ wendy.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, ... }:
+
+{
+  require = [ ./build-machines-dell-r815.nix ];
+
+  services.httpd.enable = true;
+  services.httpd.adminAddr = "e.dolstra@tudelft.nl";
+  services.httpd.enableUserDir = true;
+  services.httpd.hostName = "wendy";
+  services.httpd.extraConfig =
+    ''
+      UseCanonicalName On
+    '';
+
+  jobs.mturk_webserver_production =
+    { name = "mturk-webserver-production";
+      startOn = "started network-interfaces";
+      exec = ''
+        ${pkgs.su}/bin/su - mturk -c 'MTURK_STATE=/home/mturk/state-production WEBINTERFACE_HOME=/home/mturk/icse-2012/src/WebInterface exec /home/mturk/icse-2012/src/WebInterface/script/webinterface_server.pl >> /home/mturk/state-production/webserver.log 2>&1'
+      '';
+    };
+
+  jobs.mturk_webserver_sandbox =
+    { name = "mturk-webserver-sandbox";
+      startOn = "started network-interfaces";
+      exec = ''
+        ${pkgs.su}/bin/su - mturk -c 'WEBINTERFACE_HOME=/home/mturk/icse-2012/src/WebInterface exec /home/mturk/icse-2012/src/WebInterface/script/webinterface_server.pl -p 3001 >> /home/mturk/state-sandbox/webserver.log 2>&1'
+      '';
+    };
+
+  jobs.mturk_vnc_multiplexer_production =
+    { name = "mturk-vnc-multiplexer-production";
+      startOn = "started network-interfaces";
+      exec = ''
+        ${pkgs.su}/bin/su - mturk -c 'MTURK_STATE=/home/mturk/state-production exec vnc-multiplexer.pl >> /home/mturk/state-production/multiplexer.log 2>&1'
+      '';
+    };
+
+  jobs.mturk_vnc_multiplexer_sandbox =
+    { name = "mturk-vnc-multiplexer-sandbox";
+      startOn = "started network-interfaces";
+      exec = ''
+        ${pkgs.su}/bin/su - mturk -c 'exec vnc-multiplexer.pl >> /home/mturk/state-sandbox/multiplexer.log 2>&1'
+      '';
+    };
+
+}
