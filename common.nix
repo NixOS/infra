b59e5636ba2c72b1689c672f36b557f5b68b1d57
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-09-09 13:51:17 +0000

    * Measure the temperature of AMD CPUs.
    svn path=/configurations/trunk/tud/; revision=29129

--- common.nix
+++ common.nix
@@ -33,7 +33,7 @@
   services.zabbixAgent.server = "192.168.1.5,127.0.0.1,130.161.158.181";
   services.zabbixAgent.extraConfig =
     ''
-      UserParameter=hardware.temp.cpu.average,cat /sys/devices/platform/coretemp.*/temp1_input | ${pkgs.perl}/bin/perl -e 'while (<>) { $n++; $sum += $_; }; print $sum / $n / 1000;'
+      UserParameter=hardware.temp.cpu.average,shopt -s nullglob; cat /sys/devices/platform/coretemp.*/temp1_input /sys/bus/pci/drivers/k10temp/*/temp1_input < /dev/null | ${pkgs.perl}/bin/perl -e 'while (<>) { $n++; $sum += $_; }; print $sum / $n / 1000;'
       UserParameter=vm.memory.dirty,echo $((1024 * $(cat /proc/meminfo | sed 's/Dirty: *\([0-9]\+\) kB/\1/; t; d')))
     '';
 
