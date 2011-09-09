3265f48603774175a236b5da7f61d7d78cbc5de7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-09-09 14:18:59 +0000

    * Obsoleted by nixos-deploy-network.
    svn path=/configurations/trunk/tud/; revision=29131

--- mass-update.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh -e
-
-#export SSH_AUTH_SOCK= # hack
-
-for i in ${MACHINES:-jimmy timmy stan kyle kenny}; do
-    echo "=== Updating $i ==="
-    ssh -i /root/.ssh/id_mass_update root@$i sh --login -c '"cd /etc/nixos && svn up nixos nixpkgs configurations release && nixos-rebuild switch"'
-done
