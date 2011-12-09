dbe43b5fd99236df5f66238012ad2ff3d08b3cce
Eelco Dolstra <eelco.dolstra@logicblox.com>
2011-12-09 13:30:48 +0000

    * Oops, nixos-rebuild accidentally caused MTurk HITs to be created...
    svn path=/configurations/trunk/tud/; revision=30812

--- wendy.nix
+++ wendy.nix
@@ -16,7 +16,7 @@
     { name = "mturk-webserver-production";
       startOn = "started network-interfaces";
       exec = ''
-        ${pkgs.su}/bin/su - mturk -c 'MTURK_STATE=/home/mturk/state-production WEBINTERFACE_HOME=/home/mturk/icse-2012/src/WebInterface WEBINTERFACE_CONFIG=/home/mturk/icse-2012/src/WebInterface/webinterface-production.conf exec /home/mturk/icse-2012/src/WebInterface/script/webinterface_server.pl >> /home/mturk/state-production/webserver.log 2>&1'
+        ${pkgs.su}/bin/su - mturk -c 'MTURK_STATE=/home/mturk/state-production WEBINTERFACE_HOME=/home/mturk/icse-2012/src/WebInterface WEBINTERFACE_CONFIG=/home/mturk/icse-2012/src/WebInterface/webinterface-production.conf exec /home/mturk/icse-2012/src/WebInterface/script/webinterface_server.pl -f -k >> /home/mturk/state-production/webserver.log 2>&1'
       '';
     };
 
@@ -24,7 +24,7 @@
     { name = "mturk-webserver-sandbox";
       startOn = "started network-interfaces";
       exec = ''
-        ${pkgs.su}/bin/su - mturk -c 'WEBINTERFACE_HOME=/home/mturk/icse-2012/src/WebInterface WEBINTERFACE_CONFIG=/home/mturk/icse-2012/src/WebInterface/webinterface-sandbox.conf exec /home/mturk/icse-2012/src/WebInterface/script/webinterface_server.pl -p 3001 >> /home/mturk/state-sandbox/webserver.log 2>&1'
+        ${pkgs.su}/bin/su - mturk -c 'WEBINTERFACE_HOME=/home/mturk/icse-2012/src/WebInterface WEBINTERFACE_CONFIG=/home/mturk/icse-2012/src/WebInterface/webinterface-sandbox.conf exec /home/mturk/icse-2012/src/WebInterface/script/webinterface_server.pl -p 3001 -f -k >> /home/mturk/state-sandbox/webserver.log 2>&1'
       '';
     };
 
@@ -44,12 +44,14 @@
       '';
     };
 
+  /*
   jobs.mturk_hydra_bridge_production =
     { name = "mturk-hydra-bridge-production";
       exec = ''
         ${pkgs.su}/bin/su - mturk -c 'MTURK_STATE=/home/mturk/state-production exec create-hits-from-hydra.pl >> /home/mturk/state-production/hydra-bridge.log 2>&1'
       '';
     };
+  */
 
   jobs.mturk_vm_cleanup_production =
     { name = "mturk-vm-cleanup-production";
