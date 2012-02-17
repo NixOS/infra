70ed45c6a3acf4ced4db810a2e31bfc7455868ab
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-02-17 16:50:48 +0000

    * Use nix.gc.
    svn path=/configurations/trunk/tud/; revision=32356

--- build-machines-common.nix
+++ build-machines-common.nix
@@ -22,10 +22,9 @@ with pkgs.lib;
       build-max-silent-time = 3600
     '';
 
-  services.cron.systemCronJobs =
-    [ # Make sure that at least 100 GiB of disk space is available.
-      "15 03,09,15,21 * * * root  nix-store --gc --max-freed \"$((100 * 1024**3 - 1024 * $(df /nix/store | tail -n 1 | awk '{ print $4 }')))\" > /var/log/gc.log 2>&1"
-    ];
+  nix.gc.automatic = true;
+  nix.gc.dates = "15 03,09,15,21 * * *";
+  nix.gc.options = ''--max-freed "$((100 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | awk '{ print $4 }')))"'';
 
   networking.hostName = ""; # obtain from DHCP server
 
