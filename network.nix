5184201395e70d6e2d431580af14098f315dc995
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-03-15 23:45:01 +0000

    svn path=/configurations/trunk/tud/; revision=33129

--- network.nix
+++ network.nix
@@ -2,12 +2,12 @@
   kenny = import ./build-machines-dell-1950.nix;
   kyle = import ./build-machines-dell-1950.nix;
   hydra = import ./build-machines-dell-1950.nix;
-  stan = import ./stan.nix;
+  #stan = import ./stan.nix;
   cartman = import ./cartman.nix;
   lucifer = import ./lucifer.nix;
   wendy = import ./wendy.nix;
   ike = import ./build-machines-dell-r815.nix;
   shelley = import ./build-machines-dell-r815.nix;
-  mrkitty = import ./mrkitty.nix;
-  #mrhankey = import ./mrhankey.nix;
+  #mrkitty = import ./mrkitty.nix;
+  mrhankey = import ./mrhankey.nix;
 }
