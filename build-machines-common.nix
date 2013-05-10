d3a273f2f3b4bddbbf2dd1ec4afd09fb33303e3f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-05-10 17:20:39 +0000

    * Remove build-max-silent-time since it interferes with Hydra.
    svn path=/configurations/trunk/tud/; revision=34720

--- build-machines-common.nix
+++ build-machines-common.nix
@@ -23,11 +23,6 @@ in
       }
     ];
 
-  nix.extraOptions =
-    ''
-      build-max-silent-time = 3600
-    '';
-
   nix.gc.automatic = true;
   nix.gc.dates = "03,09,15,21:15";
   nix.gc.options = ''--max-freed "$((100 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
