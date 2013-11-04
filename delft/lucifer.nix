bd41176678cea555422895748c6b8eb169acc914
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-11-04 11:39:03 +0100

    Fix NixOS 13.10 mirror service

--- delft/lucifer.nix
+++ delft/lucifer.nix
@@ -135,15 +135,15 @@
       serviceConfig.CPUShares = 100;
     };
 
-  systemd.services."mirror-nixos-13.10" =
-    { description = "Mirror NixOS 13.10";
-      wantedBy = [ "multi-user.target" ];
+  systemd.services.mirror-nixos-stable =
+    { description = "Mirror NixOS Stable";
+      #wantedBy = [ "multi-user.target" ];
       after = [ "networking.target" ];
       path = [ pkgs.su ];
       script =
         ''
           rm -rf /data/releases/nixos/13.10/.tmp-*
-          exec su - hydra-mirror -c 'cd release/channels; while true; do ./mirror-nixos-branch.sh 13.10 release-13.10; sleep 1200; done'
+          exec su - hydra-mirror -c 'cd release/channels; while true; do ./mirror-nixos-stable.sh; sleep 1200; done'
         '';
       serviceConfig.Restart = "always";
       serviceConfig.CPUShares = 100;
