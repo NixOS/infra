acd3cb67d83760bb6f1f74154f1cc36c2157245e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-11-04 11:37:32 +0100

    Make sure that the bridge on stan is maintained after nixos-rebuild

--- delft/stan.nix
+++ delft/stan.nix
@@ -22,22 +22,17 @@
 
   networking.bridges.veno1.interfaces = [ config.system.build.mainPhysicalInterface ];
 
+  # Libvirt dynamically creates vnet* interfaces to connect interfaces
+  # to the veno1 bridge.  So after restarting veno1, we have to add
+  # those interfaces back in.
+  systemd.services.veno1.postStart =
+    ''
+      for iface in $(cd /sys/class/net && echo vnet*); do
+        brctl addif veno1 "$iface" || true
+      done
+    '';
+
   system.build.mainVirtualInterface = "veno1";
 
   virtualisation.libvirtd.enable = true;
-
-  /*
-  networking.localCommands =
-    ''
-      # Enable IPv6 forwarding.  Nova/radvd requires this.
-      echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
-
-      # Urgh.  The Linux kernel disables processing of router
-      # advertisements if forwarding is enabled, so we have to
-      # configure manually.  Apparently in newer kernels, we can set
-      # /proc/sys/net/ipv6/conf/all/accept_ra to 2 instead.
-      ip -6 addr add 2001:610:685:1:222:19ff:fe55:bf2e/64 dev veno1 || true
-      ip -6 route add default via fe80::204:23ff:fedf:f7bf dev veno1 || true
-    '';
-  */
 }
