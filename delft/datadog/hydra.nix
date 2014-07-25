fccf86e372a402b3e59178b972789deaf52dee2a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-25 15:22:01 +0200

    Add stuff

---
+++ delft/datadog/hydra.nix
@@ -0,0 +1,21 @@
+{config, pkgs, ...}:
+{
+  systemd.services.dd-agent.environment.PYTHONPATH = "${pkgs.pythonPackages.requests}/lib/python2.7/site-packages";
+  environment.etc =
+    let
+      hydra-config =
+        pkgs.writeText "hydra.yaml" ''
+          init_config:
+    
+          instances:
+            - check: 1
+        '';
+    in [
+      { source = hydra-config;
+        target = "dd-agent/conf.d/hydra.yaml";
+      }
+      { source = ./hydra.py;
+        target = "dd-agent/checks.d/hydra.py";
+      }
+    ];
+}
---
+++ delft/datadog/hydra.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+from checks import *
+import os
+import subprocess
+import requests
+import json
+
+class HydraCheck(AgentCheck):
+    def check(self, instance):
+        r = requests.get('http://localhost:3000/status', headers={'Content-Type':'application/json'})
+        self.gauge('hydra.active_buildsteps', len(json.loads(r.text)))
+
