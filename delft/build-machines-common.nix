f4c9805e9e489db5f2e6a8d68882dae411cf5961
Rob Vermaas <rob.vermaas@gmail.com>
2015-06-12 09:40:41 +0000

    Remove old maven stuff.

--- delft/build-machines-common.nix
+++ delft/build-machines-common.nix
@@ -1,15 +1,6 @@
 { config, pkgs, ... }:
 
 with pkgs.lib;
-let
-  m3chown = pkgs.writeScript "chown-tmp-m3" ''
-    #! /bin/sh
-    set -e
-    if [[ -d /tmp/m3 ]]; then
-      chmod ug+w -R /tmp/m3
-    fi
-  '';
-in
 {
   require = [ ./common.nix ];
 
@@ -26,10 +17,6 @@ in
   nix.gc.automatic = true;
   nix.gc.dates = "03,09,15,21:15";
   nix.gc.options = ''--max-freed "$((100 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
-
-  services.cron.systemCronJobs = [
-    "0,30 * * * * ${m3chown}"
-  ];
 
   users.extraUsers.root.openssh.authorizedKeys.keys = singleton
     ''
