6a602ec00dd56c7b83c7d69a421d9f40e9ff3b2b
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-02-19 17:51:38 +0100

    Bump GC max-freed

--- delft/build-machines-common.nix
+++ delft/build-machines-common.nix
@@ -16,7 +16,7 @@ with pkgs.lib;
 
   nix.gc.automatic = true;
   nix.gc.dates = "03,09,15,21:15";
-  nix.gc.options = ''--max-freed "$((100 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
+  nix.gc.options = ''--max-freed "$((128 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
 
   users.extraUsers.root.openssh.authorizedKeys.keys = singleton
     ''
