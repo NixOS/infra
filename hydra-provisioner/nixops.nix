588e0f4c509ec5f9df58fce726cb6169e794af96
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-04 14:26:06 +0000

    Set nix.buildCores
    Fixes #15.

--- hydra-provisioner/nixops.nix
+++ hydra-provisioner/nixops.nix
@@ -39,6 +39,8 @@ in
 
       nix.package = pkgs.nixUnstable;
 
+      nix.buildCores = 0;
+
       nix.gc.automatic = true;
       nix.gc.dates = "*:0/30";
       nix.gc.options = ''--max-freed "$((15 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
