07a9204f01d38d1edd2df3fcc28823c6c4c804f4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-04 14:24:24 +0000

    Enable auto shutdown

--- hydra-provisioner/nixops.nix
+++ hydra-provisioner/nixops.nix
@@ -27,11 +27,13 @@ in
       deployment.ec2.ebsInitialRootDiskSize = 100;
       deployment.owners = [ "eelco.dolstra@logicblox.com" ];
 
-
-      imports = [ <nixpkgs/nixos/modules/profiles/minimal.nix> ];
+      imports =
+        [ <nixpkgs/nixos/modules/profiles/minimal.nix>
+          <hydra-provisioner/auto-shutdown.nix>
+        ];
 
       users.extraUsers.root.openssh.authorizedKeys.keys =
-        [ ''command="nix-store --serve --write" ${builtins.readFile ~/.ssh/id_dsa.pub} # for testing''
+        [ # ''command="nix-store --serve --write" ${builtins.readFile ~/.ssh/id_dsa.pub} # for testing
           ''command="nix-store --serve --write" ssh-dss AAAAB3NzaC1kc3MAAACBAMHRjGSDaBp4Z30JF4S9ApabBCpdr57Ad0aD9oH2A/WEFnWYQSAzK4E/HHD2DV2XP1stNkZ1ks2v3F4Yu/veR+qVlUWbJW1RIIfuQgkG44K0R3C2qx4BAZUVYzju1NVCJbBOO6ipVY9cfmpokV52HZFhP/2HocTNLoav3F0AsbbJAAAAFQDaJiQdpJBEa4Wr5FfVl1kYqmQZJwAAAIEAwbern5XL+SNIMa+sJ3CBhrWyYExYWiUbdmhQEfyEAUmoPsEr1qpb+0WREic9Nrxz48QWZDK5xMvzZyQEkuAMJUBWcdm12rME7WMvg7OZGr9DADjAtfMfj3Ui2XvOuQ3ia/OTsMGkQTDWnkOM9Ni128SNSl9urFBlXATdGvo+468AAACBAK8s6LddhhkRqsF/l/L2ooS8c8A1rTFWAOy3/sgXFNvMyS/Mig2p966xRrRHr7Bc+H2SuKEE5WmLCXqymgxLHhrFU4zm/W/ej1yB1CAThd4xUfgJu4touJROjvcD1zzlmLeat0fp2k5mCuiLKcTKi0vxKWiiopF9nvBBK+7ODPC7 buildfarm@nixos''
         ];
 
