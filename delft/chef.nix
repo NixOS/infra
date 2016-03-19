23f5acf14b59e8956ad968e93637e17a20614b34
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-19 12:28:34 +0100

    Enable automatic GC on chef

--- delft/chef.nix
+++ delft/chef.nix
@@ -42,4 +42,7 @@
 
   };
 
+  nix.gc.automatic = true;
+  nix.gc.options = ''--max-freed "$((50 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
+
 }
