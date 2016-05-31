aa3ea4a67ff78554e2d9795a068dfb23cfd82610
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-05-31 09:35:26 +0000

    Enable chroot builds on EC2 instances

--- hydra-provisioner/nixops.nix
+++ hydra-provisioner/nixops.nix
@@ -42,6 +42,8 @@ in
 
       nix.buildCores = 0;
 
+      nix.useChroot = true;
+
       nix.gc.automatic = true;
       nix.gc.dates = "*:0/30";
       nix.gc.options = ''--max-freed "$((15 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
