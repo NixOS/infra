a122f418797713d519aadf02e677fce0dc1cb446
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-08-12 16:55:06 +0200

    Disable pre-build-hook on the Macs
    The pre-build-hook segfaults and we don't need it because we're not
    doing sandbox builds.

---
+++ delft/scripts/nix-mac-nuke.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+service_plist=/Library/LaunchDaemons/org.nixos.nix-daemon.plist
+
+launchctl stop $service_plist
+launchctl unload $service_plist
+
+dscl . -delete /Groups/nixbld
+
+for i in $(seq 1 20); do
+    dscl . -delete /Users/nixbld$i
+done
+
+sudo rm -f $service_plist
+
+sudo rm -rf /nix /etc/nix/nix.conf
+
+rm -f $HOME/.nix-channels $HOME/.nix-profile
+rm -rf $HOME/.nix-defexpr
