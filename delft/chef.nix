16119060c623d5621ee010baa49ce734e57c1f4b
Eelco Dolstra <edolstra@gmail.com>
2016-11-15 14:59:01 +0100

    Do more aggressive GC

--- delft/chef.nix
+++ delft/chef.nix
@@ -46,6 +46,8 @@
   nix.gc.options = ''--max-freed "$((100 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
   nix.gc.dates = "03,09,15,21:15";
 
+  nix.extraOptions = "gc-keep-outputs = false";
+
   networking.defaultMailServer.directDelivery = lib.mkForce false;
   services.postfix.enable = true;
   services.postfix.hostname = "hydra.nixos.org";
