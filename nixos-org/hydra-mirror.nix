1449ab26cfcc0cb3b243cfe4d9144783e2041d7c
Graham Christensen <graham@grahamc.com>
2017-03-05 22:00:57 -0500

    Pass GITHUB_OAUTH_TOKEN for https://github.com/NixOS/nixos-channel-scripts/pull/11

--- nixos-org/hydra-mirror.nix
+++ nixos-org/hydra-mirror.nix
@@ -23,6 +23,7 @@ let
               # FIXME: use IAM role.
               export AWS_ACCESS_KEY_ID=$(sed 's/aws_access_key_id=\(.*\)/\1/ ; t; d' ~/.aws/credentials)
               export AWS_SECRET_ACCESS_KEY=$(sed 's/aws_secret_access_key=\(.*\)/\1/ ; t; d' ~/.aws/credentials)
+              export GITHUB_OAUTH_TOKEN=$(cat ~/.github/token)
               exec mirror-nixos-branch ${channelName} https://hydra.nixos.org/job/${mainJob}/latest-finished
             ''; # */
           serviceConfig.User = "hydra-mirror";
