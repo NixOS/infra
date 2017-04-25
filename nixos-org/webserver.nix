NixOS/infra

commit 045e81f3da06239665bec613451b4d9358a81fdd

Disable access to .git and other dot files

nixos-org/webserver.nix
@@ -45,6 +45,11 @@ let
           Redirect /tarballs http://tarballs.nixos.org
           Redirect /releases/nixos https://d3g5gsiof5omrk.cloudfront.net/nixos
 
+          # Don't allow access to dot files (like .git).
+          RewriteEngine on
+          RewriteCond %{THE_REQUEST} ^.*/\.
+          RewriteRule ^(.*)$ - [R=404]
+
           <Location /server-status>
             SetHandler server-status
             Allow from 127.0.0.1

commit f84b1720b39e4d745121199c026efd82998263b4

Don't block all dot files, only .git
045e81f3da06239665bec613451b4d9358a81fdd (maybe just in conjunction
with httpd 2.4) broke ACME because it blocked access to
.well-known/acme-challenge, giving errors such as

  AH00124: Request exceeded the limit of 10 internal redirects due to probable configuration error. Use 'LimitInternalRecursion' to increase the limit if necessary. Use 'LogLevel debug' to get a backtrace.

nixos-org/webserver.nix
@@ -46,10 +46,9 @@ let
           Redirect /tarballs http://tarballs.nixos.org
           Redirect /releases/nixos https://d3g5gsiof5omrk.cloudfront.net/nixos
 
-          # Don't allow access to dot files (like .git).
+          # Don't allow access to .git directories.
           RewriteEngine on
-          RewriteCond %{THE_REQUEST} ^.*/\.
-          RewriteRule ^(.*)$ - [R=404]
+          RewriteRule "^(.*/)?\.git/" - [F,L]
 
           <Location /server-status>
             SetHandler server-status