92620f5c9134d69a096faca422a3310702e2adf6
Eelco Dolstra <edolstra@gmail.com>
2017-07-27 12:50:24 +0200

    More zabbix removal

--- delft/megacli.nix
+++ delft/megacli.nix
@@ -6,10 +6,4 @@
     ''
       zabbix ALL=(ALL) NOPASSWD: ${pkgs.megacli}/bin/MegaCli64
     '';
-
-  services.zabbixAgent.extraConfig =
-    ''
-      UserParameter=megaraid[*],/var/setuid-wrappers/sudo ${pkgs.megacli}/bin/MegaCli64 -pdInfo -PhysDrv[$2:$3] -a$1 | grep '$4' | cut -f2 -d':' | cut -b2-
-      UserParameter=megaraid.degraded,/var/setuid-wrappers/sudo ${pkgs.megacli}/bin/MegaCli64 -AdpAllInfo -aAll -NoLog | grep -A 2 'Virtual Drives' | grep Degraded | ${pkgs.gawk}/bin/awk '/Degraded/ {TOTAL += $3} END {print TOTAL}'
-    '';
 }
