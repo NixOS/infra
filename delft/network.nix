54d80e5185348b8686e27afd7c91732328b52f9d
Eelco Dolstra <edolstra@gmail.com>
2018-08-01 11:23:26 +0200

    Doh

--- delft/network.nix
+++ delft/network.nix
@@ -2,6 +2,6 @@
   hydra = { deployment.targetHost = "hydra.ewi.tudelft.nl"; imports = [ ./build-machines-dell-1950.nix ]; };
   lucifer = { deployment.targetHost = "lucifer.ewi.tudelft.nl"; imports = [ ./lucifer.nix ]; };
   wendy = { deployment.targetHost = "wendy.ewi.tudelft.nl"; imports = [ ./wendy.nix ]; };
-  ike = { deployment.targetHost = "wendy.ewi.tudelft.nl"; imports = [ ./build-machines-dell-r815.nix ]; };
+  ike = { deployment.targetHost = "ike.ewi.tudelft.nl"; imports = [ ./build-machines-dell-r815.nix ]; };
   chef = import ./chef.nix;
 }
