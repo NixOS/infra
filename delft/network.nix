7b21a73a823b5e0baee59d634080eb00b08a6ee5
graham christensen <graham@grahamc.com>
2018-10-30 12:24:39 +0100

    Import packet-epyc-1

--- delft/network.nix
+++ delft/network.nix
@@ -3,6 +3,7 @@
   lucifer = { deployment.targetHost = "lucifer.ewi.tudelft.nl"; imports = [ ./lucifer.nix ]; };
   wendy = { deployment.targetHost = "wendy.ewi.tudelft.nl"; imports = [ ./wendy.nix ]; };
   ike = { deployment.targetHost = "ike.ewi.tudelft.nl"; imports = [ ./build-machines-dell-r815.nix ]; };
+  packet-epyc-1 = { deployment.targetHost = "147.75.198.47"; imports = [ ./packet/packet-epyc-1.nix ./build-machines-common.nix ]; };
   chef = import ./chef.nix;
   eris = import ./eris.nix;
 }
