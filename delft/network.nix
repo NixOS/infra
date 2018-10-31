93b331685751894022ec39e4036279824871c841
graham christensen <graham@grahamc.com>
2018-10-31 10:30:15 +0100

    Finish importing packet-t2-4 in to the network

--- delft/network.nix
+++ delft/network.nix
@@ -4,6 +4,7 @@
   wendy = { deployment.targetHost = "wendy.ewi.tudelft.nl"; imports = [ ./wendy.nix ]; };
   ike = { deployment.targetHost = "ike.ewi.tudelft.nl"; imports = [ ./build-machines-dell-r815.nix ]; };
   packet-epyc-1 = { deployment.targetHost = "147.75.198.47"; imports = [ ./packet/packet-epyc-1.nix ./build-machines-common.nix ]; };
+  packet-t2-4 = { deployment.targetHost = "147.75.98.145"; imports = [ ./packet/packet-t2-4.nix ./build-machines-common.nix ]; };
   chef = import ./chef.nix;
   eris = import ./eris.nix;
 }
