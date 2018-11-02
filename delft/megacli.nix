c267f9fd51cced98b8f791d402190eea968fed0a
graham christensen <graham@grahamc.com>
2018-11-02 12:02:48 +0100

    Delete zabbix reference

--- delft/megacli.nix
+++ delft/megacli.nix
@@ -1,9 +1,4 @@
 { config, pkgs, ...}:
 {
   environment.systemPackages = [ pkgs.megacli ];
-
-  security.sudo.configFile =
-    ''
-      zabbix ALL=(ALL) NOPASSWD: ${pkgs.megacli}/bin/MegaCli64
-    '';
 }
