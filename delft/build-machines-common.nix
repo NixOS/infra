6731dfee379fcbeb1c115da7f0a9f7a7d1c3e462
graham christensen <graham@grahamc.com>
2018-11-02 12:02:37 +0100

    Increase GC frequency

--- delft/build-machines-common.nix
+++ delft/build-machines-common.nix
@@ -6,7 +6,7 @@ with lib;
   imports = [ ./common.nix ];
 
   nix.gc.automatic = true;
-  nix.gc.dates = "03,09,15,21:15";
+  nix.gc.dates = "*:45";
   nix.gc.options = ''--max-freed "$((128 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
 
   # Randomize GC start times do we don't block all build machines at
