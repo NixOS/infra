4196be086315ac38973684f3ac1e252ec93ec2bf
Graham Christensen <graham@grahamc.com>
2019-09-28 20:35:29 +0200

    packet-sd: upgrade for short_id metadata

--- delft/prometheus/packet-sd.nix
+++ delft/prometheus/packet-sd.nix
@@ -1,16 +1,16 @@
 { buildGoModule, fetchFromGitHub, lib }:
 buildGoModule rec {
   pname = "prometheus-packet-sd";
-  version = "0.0.2";
+  version = "2019-09-28";
 
   src = fetchFromGitHub {
     owner = "packethost";
     repo = "prometheus-packet-sd";
-    rev = "v${version}";
-    sha256 = "0y2c73irk1xy3raha9yx1j5ja5h2phalnwjcbsgpr6jnfs0v6shh";
+    rev = "2944af336b2fda49d5840d6cc28877afad0fd031";
+    sha256 = "01464m88pcv0zfd6hm4m02phf5b9gj2k04qdd71iir4zvvjgyd6g";
   };
 
-  modSha256 = "1cgyfc77fdrmv7f2cy0qpwgmn9wpmaa66806brflkpkq1nc9dr5b";
+  modSha256 = "1km8hl7lh71bvkz3w4bdi2zpwar1fdvxpaa7nkjq8fvlyki16f33";
 
   subPackages = [ "." ];
 
