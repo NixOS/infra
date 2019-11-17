7fbae42e741f3a0f8118327359491616f79637ef
Eelco Dolstra <edolstra@gmail.com>
2019-11-17 15:26:17 +0100

    prom alert: change stuck builds from 1 to 2 days

--- delft/eris.nix
+++ delft/eris.nix
@@ -63,8 +63,8 @@ in {
           name = "hydra";
           rules = [
             {
-              alert = "BuildsStuckOverOneDay";
-              expr = ''hydra_machine_build_duration_bucket{le="172800"} - ignoring(le) hydra_machine_build_duration_bucket{le="86400"} > 0'';
+              alert = "BuildsStuckOverTwoDays";
+              expr = ''hydra_machine_build_duration_bucket{le="259200"} - ignoring(le) hydra_machine_build_duration_bucket{le="172800"} > 0'';
               labels.severity = "page";
               annotations.summary = "https://status.nixos.org/grafana/d/j0hJAY1Wk/in-progress-build-duration-heatmap";
             }
