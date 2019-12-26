e7e22664ad88fde70647bc3ab2ebb31a1c81420a
Eelco Dolstra <edolstra@gmail.com>
2019-12-26 03:32:47 +0100

    channels: move to a .nix file for referencing elsewhere

--- modules/hydra-mirror.nix
+++ modules/hydra-mirror.nix
@@ -1,24 +1,7 @@
 { lib, pkgs, ... }:
 
 let
-  channels = {
-      # Channel name           https://hydra.nixos.org/job/<value>/latest-finished
-      "nixos-unstable"       = "nixos/trunk-combined/tested";
-      "nixos-unstable-small" = "nixos/unstable-small/tested";
-      "nixpkgs-unstable"     = "nixpkgs/trunk/unstable";
-
-      "nixos-19.09"          = "nixos/release-19.09/tested";
-      "nixos-19.09-small"    = "nixos/release-19.09-small/tested";
-      "nixpkgs-19.09-darwin" = "nixpkgs/nixpkgs-19.09-darwin/darwin-tested";
-
-      "nixos-19.03"          = "nixos/release-19.03/tested";
-      "nixos-19.03-small"    = "nixos/release-19.03-small/tested";
-      "nixpkgs-19.03-darwin" = "nixpkgs/nixpkgs-19.03-darwin/darwin-tested";
-
-      "nixos-18.09"          = "nixos/release-18.09/tested";
-      "nixos-18.09-small"    = "nixos/release-18.09-small/tested";
-      "nixpkgs-18.09-darwin" = "nixpkgs/nixpkgs-18.09-darwin/darwin-tested";
-  };
+  channels = (import ../channels.nix).channels;
 
   channelScripts = import /home/deploy/src/nixos-channel-scripts { inherit pkgs; }; # FIXME
   orderLib = import ../lib/service-order.nix { inherit lib; };
