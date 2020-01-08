32947059eebc33282b7dcfead2431a08b59943b7
Daiderd Jordan <daiderd@gmail.com>
2020-01-09 00:02:07 +0100

    status-page: add alerts for github issues (#93)
    * status-page: add alerts for github issues
    
    * status-page: tweak css

--- delft/eris/status-page/status.js
+++ delft/eris/status-page/status.js
@@ -8,6 +8,30 @@ function init() {
   });
 }
 
+async function fetchIssues(label) {
+    const response = await fetch(`https://api.github.com/repos/NixOS/nixpkgs/issues?labels=${label}`);
+    return await response.json();
+}
+
+fetchIssues("1.severity%3A%20channel%20blocker")
+    .then(data => data.map(issue => {
+        var el = document.createElement('div');
+        el.classList = "alert alert-warning";
+        el.innerHTML = '<span class="issue-age"></span> <a class="issue-link"></a>';
+        const since = moment(issue['created_at']).fromNow();
+        el.getElementsByClassName('issue-age')[0].innerText = since;
+        el.getElementsByClassName('issue-link')[0].href = issue['html_url'];
+        el.getElementsByClassName('issue-link')[0].innerText = issue['title'];
+        if (issue['labels'].find(label => label['name'] == 'infrastructure')) {
+            el.innerHTML += ' <span class="label label-important">Infrastructure</span>'
+        }
+        return el;
+    }))
+    .then(elems => {
+        var alerts = document.getElementById('alerts');
+        elems.forEach(el => alerts.appendChild(el));
+    });
+
 function aggregateByChannel(result) {
   return result.reduce((acc, {
     channel,
@@ -18,7 +42,7 @@ function aggregateByChannel(result) {
   }), {});
 }
 
-async function fetchData(queryType, queryArgs = {}) {
+async function fetchMetrics(queryType, queryArgs = {}) {
   const params = new URLSearchParams();
   for (const [key, value] of Object.entries(queryArgs)) {
     params.set(key, String(value));
@@ -32,7 +56,7 @@ async function fetchData(queryType, queryArgs = {}) {
   return data.result;
 }
 
-const revisionData = fetchData('query', {
+const revisionData = fetchMetrics('query', {
     query: 'channel_revision'
   })
   .then(records => (
@@ -50,7 +74,7 @@ const revisionData = fetchData('query', {
   .then(aggregateByChannel);
 
 
-const updateTimeData = fetchData('query', {
+const updateTimeData = fetchMetrics('query', {
     query: 'channel_update_time'
   })
   .then(records => (
@@ -75,7 +99,7 @@ if (idealStart > earliestStart) {
     start = earliestStart;
 }
 var end = moment.utc().format();
-const jobsetData = fetchData('query_range', {
+const jobsetData = fetchMetrics('query_range', {
     query: 'hydra_job_failed',
     start: start.format(),
     end,
