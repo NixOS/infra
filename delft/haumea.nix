52748f7f36641890080fc81332a5ae9a2140ac0c
Graham Christensen <graham@grahamc.com>
2020-03-20 16:59:28 +0100

    haumea: break up znapzend destinations before adding a second

--- delft/haumea.nix
+++ delft/haumea.nix
@@ -168,10 +168,12 @@
         plan = "15min=>5min,1hour=>15min,1day=>1hour,4day=>1day,3week=>1week";
         recursive = true;
         timestampFormat = "%Y-%m-%dT%H:%M:%SZ";
-        destinations.ogden = {
-          plan = "1hour=>5min,4day=>1hour,1week=>1day,1year=>1week,10year=>1month";
-          host = "hydraexport@lord-nibbler.gsc.io";
-          dataset = "rpool/backups/nixos.org/haumea/safe";
+        destinations = {
+          ogden = {
+            plan = "1hour=>5min,4day=>1hour,1week=>1day,1year=>1week,10year=>1month";
+            host = "hydraexport@lord-nibbler.gsc.io";
+            dataset = "rpool/backups/nixos.org/haumea/safe";
+          };
         };
       };
     };
