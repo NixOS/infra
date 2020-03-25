07de446010eb9f00b6c484bc7ce9743a0f7d4468
Graham Christensen <graham@grahamc.com>
2020-03-25 01:01:29 +0100

    macs: GC every hour

--- macs/guest/darwin-configuration.nix
+++ macs/guest/darwin-configuration.nix
@@ -31,6 +31,7 @@ in
   nix.maxJobs = 4;
   nix.buildCores = 1;
   nix.gc.automatic = true;
+  nix.gc.interval = { Minute = 15; };
   nix.gc.options = let
       gbFree = 50;
   in "--max-freed $((${toString gbFree} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | awk '{ print $4 }')))";
