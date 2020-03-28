b20b4213f941f1b3adb58739f96ecaa04553baa1
Eelco Dolstra <edolstra@gmail.com>
2020-03-28 18:39:34 +0100

    bastion: Remove nix override

--- bastion/flake.nix
+++ bastion/flake.nix
@@ -4,7 +4,6 @@
   inputs.nixpkgs.url = "nixpkgs/release-19.09";
   #inputs.nixops.url = "/home/deploy/src/nixops";
   inputs.nixos-channel-scripts.url = github:NixOS/nixos-channel-scripts;
-  inputs.nix.url = github:NixOS/nix/cfc38257cfcdabd34151d723906b38873e7ef6d0;
 
   outputs = { self, nixpkgs, nix, nixops, nixos-channel-scripts }: {
 
