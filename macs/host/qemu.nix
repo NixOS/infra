fa66de309a214edf08e0914023416f0341950968
Graham Christensen <graham@grahamc.com>
2020-04-02 22:50:17 +0200

    20.03 broke kresd.service

--- macs/host/qemu.nix
+++ macs/host/qemu.nix
@@ -27,7 +27,7 @@ in {
     };
 
     systemd.services."run-macos-vm" = rec {
-      requires = [ "create-macos-secrets.service" "dhcpd4.service" "kresd.service" "network-online.target" ];
+      requires = [ "create-macos-secrets.service" "dhcpd4.service" "kresd@1.service" "network-online.target" ];
       after = requires;
       wantedBy = [ "multi-user.target" ];
       wants = [ "netcatsyslog.service" "healthcheck-macos-vm.timer" ];
