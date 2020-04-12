267186a9c80393148a587ea60216d4f720cd7351
Eelco Dolstra <edolstra@gmail.com>
2020-04-12 16:53:10 +0200

    bastion/flake.lock: Update
    Flake input changes:
    
    * Updated 'nixops/nixpkgs': 'github:NixOS/nixpkgs/29eddfc36d720dcc4822581175217543b387b1e8' -> 'github:NixOS/nixpkgs/66d8a2bda511e1fc46725322f94261c8a4083757'

--- bastion/flake.nix
+++ bastion/flake.nix
@@ -3,6 +3,7 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-20.03-small";
   inputs.nixos-channel-scripts.url = github:NixOS/nixos-channel-scripts;
+  inputs.nixops.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, nix, nixops, nixos-channel-scripts }: {
 
