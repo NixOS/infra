7a30ea9ca476c2e004fa214dc2aa498b2d57aaa0
Eelco Dolstra <edolstra@gmail.com>
2020-07-28 12:14:34 +0200

    Follow hydra's nix input

--- delft/flake.nix
+++ delft/flake.nix
@@ -1,6 +1,7 @@
 {
   #inputs.nixpkgs.url = "/home/deploy/src/edolstra-nixpkgs"; # toString ../../edolstra-nixpkgs; # = "nixpkgs/release-19.09";
   inputs.nixpkgs.url = "nixpkgs/nixos-20.03";
+  inputs.nix.follows = "hydra/nix";
 
   outputs = flakes @ { self, nixpkgs, nix, hydra, dwarffs }: {
 
