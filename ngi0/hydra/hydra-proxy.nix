a0b3a0cb0dcbfb1aa7725e4fdbb5939c9d1adcd9
Eelco Dolstra <edolstra@gmail.com>
2020-10-26 12:27:55 +0100

    Remove ACME stuff

--- ngi0/hydra/hydra-proxy.nix
+++ ngi0/hydra/hydra-proxy.nix
@@ -30,9 +30,6 @@ let
       </Location>
     '';
 
-  acmeKeyDir = "/var/lib/acme/hydra.ngi0.nixos.org";
-  acmeWebRoot = "/var/lib/httpd/acme";
-
 in
 
 {
@@ -50,8 +47,7 @@ in
 
     virtualHosts."hydra.ngi0.nixos.org" =
       { addSSL = true;
-        sslServerKey = "${acmeKeyDir}/key.pem";
-        sslServerCert = "${acmeKeyDir}/fullchain.pem";
+        enableACME = true;
         extraConfig = ''
           # Required by Catalyst.
           RequestHeader set X-Forwarded-Proto https
@@ -62,35 +58,11 @@ in
           [ { urlPath = "/apache-errors";
               dir = ../../delft/apache-errors;
             }
-            { urlPath = "/.well-known/acme-challenge";
-              dir = "${acmeWebRoot}/.well-known/acme-challenge";
-            }
           ];
       };
 
   };
 
-  # Let's Encrypt configuration.
   security.acme.acceptTerms = true;
-  security.acme.certs."hydra.ngi0.nixos.org" =
-    { email = "ngi@nixos.org";
-      webroot = acmeWebRoot;
-      postRun = "systemctl reload httpd.service";
-    };
-
-  # Generate a dummy self-signed certificate until we get one from
-  # Let's Encrypt.
-  system.activationScripts.createDummyKey =
-    ''
-      dir=${acmeKeyDir}
-      mkdir -m 0700 -p $dir
-      if ! [[ -e $dir/key.pem ]]; then
-        ${pkgs.openssl}/bin/openssl genrsa -passout pass:foobar -des3 -out $dir/key-in.pem 1024
-        ${pkgs.openssl}/bin/openssl req -passin pass:foobar -new -key $dir/key-in.pem -out $dir/key.csr \
-          -subj "/C=NL/ST=Denial/L=Springfield/O=Dis/CN=www.example.com"
-        ${pkgs.openssl}/bin/openssl rsa -passin pass:foobar -in $dir/key-in.pem -out $dir/key.pem
-        ${pkgs.openssl}/bin/openssl x509 -req -days 365 -in $dir/key.csr -signkey $dir/key.pem -out $dir/fullchain.pem
-      fi
-    '';
-
+  security.acme.email = "ngi@nixos.org";
 }
