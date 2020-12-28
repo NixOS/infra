bb79b3f085c759b07500da3c0208aefbf45f4c66
Graham Christensen <graham@grahamc.com>
2020-12-28 19:36:31 +0100

    Fetch and push rfc39 records for the team sync

--- modules/rfc39.nix
+++ modules/rfc39.nix
@@ -13,6 +13,10 @@ in {
     user = "rfc39";
   };
 
+  deployment.keys."rfc39-record-push.key" = {
+    keyFile = /home/deploy/src/nixos-org-configurations/keys/rfc39-record-push.key;
+    user = "rfc39";
+  };
 
   users.extraUsers.rfc39 = {
     description = "RFC39 Maintainer Team Sync";
@@ -20,10 +24,11 @@ in {
     createHome = true;
   };
 
+  programs.ssh.knownHosts."github.com".publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==";
+
   systemd.services.rfc39-sync = {
-    enable = false;
     description = "Sync the Maintainer Team ";
-    path  = [ config.nix.package pkgs.git rfc39 ];
+    path  = [ config.nix.package pkgs.git pkgs.openssh rfc39 ];
     startAt = "*:0/30";
     serviceConfig.User = "rfc39";
     serviceConfig.Group = "keys";
@@ -33,20 +38,42 @@ in {
         ''
           set -eux
 
-          dir=$HOME/nixpkgs
-          if ! [[ -e $dir ]]; then
-            git clone https://github.com/NixOS/nixpkgs.git $dir
+          export GIT_SSH_COMMAND='ssh -i /run/keys/rfc39-record-push.key'
+          export GIT_AUTHOR_NAME="rfc39"
+          export GIT_AUTHOR_EMAIL="rfc39@eris"
+          export GIT_COMMITTER_NAME="rfc39"
+          export GIT_COMMITTER_EMAIL="rfc39@eris"
+
+          recordsdir=$HOME/rfc39-record
+          if ! [[ -e "$recordsdir" ]]; then
+            git clone git@github.com:NixOS/rfc39-record.git "$recordsdir"
           fi
-          cd $dir
+          cd "$recordsdir"
+          git remote update origin
+          git checkout main
+          git reset --hard origin/main
+          git gc
+
+          nixpkgsdir=$HOME/nixpkgs
+          if ! [[ -e $nixpkgsdir ]]; then
+            git clone https://github.com/NixOS/nixpkgs.git $nixpkgsdir
+          fi
+          cd $nixpkgsdir
           git remote update origin
           git checkout origin/master
           git gc
 
-          exec rfc39 \
+          rfc39 \
               --dump-metrics --metrics-delay=240 --metrics-addr=0.0.0.0:9190 \
               --credentials /run/keys/rfc39-credentials.nix \
               --maintainers ./maintainers/maintainer-list.nix \
-              sync-team NixOS 3345117 --limit 10
+              sync-team NixOS 3345117 --limit 10 \
+              --invited-list "$recordsdir/invitations"
+
+          cd "$recordsdir"
+          git add .
+          git commit -m "Automated team sync results."
+          git push origin main
         '';
     };
 
