18db094416b90100dcbabf69d158b8f8e951dc30
Eelco Dolstra <edolstra@gmail.com>
2021-04-22 10:57:23 +0200

    bastion: Add some follows

--- bastion/flake.nix
+++ bastion/flake.nix
@@ -1,7 +1,10 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixos-20.09-small";
   inputs.nixos-channel-scripts.url = github:NixOS/nixos-channel-scripts;
+  inputs.nixos-channel-scripts.inputs.nix.follows = "nix";
+  inputs.nixos-channel-scripts.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixops.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nix.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, nix, nixops, nixos-channel-scripts }: {
 
