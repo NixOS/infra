93f42d6a4a1aec223c223834aba640aa8a73cf49
zimbatm <zimbatm@zimbatm.com>
2021-09-03 10:49:52 +0200

    tf: add diagnostic script to the repo
    Amend the original diagnostic script to make it more generic, and copy
    that into this repo so that everybody can propose changes to it.

---
+++ terraform/cache/diagnostic.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p bind.dnsutils -p mtr -p curl
+# shellcheck shell=bash
+# impure: needs ping
+#
+# Run this script if you are having issues with cache.nixos.org and paste the
+# output URL in a new issue in the same repo.
+#
+
+domain=${1:-cache.nixos.org}
+
+run() {
+  echo "> $*"
+  "$@" |& sed -e "s/^/    /"
+  printf "Exit: %s\n\n\n" "$?"
+}
+
+curl_w="
+time_namelookup:    %{time_namelookup}
+time_connect:       %{time_connect}
+time_appconnect:    %{time_appconnect}
+time_pretransfer:   %{time_pretransfer}
+time_redirect:      %{time_redirect}
+time_starttransfer: %{time_starttransfer}
+time_total:         %{time_total}
+"
+
+curl_test() {
+  curl -w "$curl_w" -v -o /dev/null "$@"
+}
+
+ix() {
+  url=$(cat | curl -F 'f:1=<-' ix.io 2> /dev/null)
+  echo "Pasted at: $url"
+}
+
+(
+  echo "domain=$domain"
+  run dig -t A "$domain"
+  run ping -c1 "$domain"
+  run ping -4 -c1 "$domain"
+  run ping -6 -c1 "$domain"
+  run mtr -c 20 -w -r "$domain"
+  run curl_test -4 "http://$domain/"
+  run curl_test -6 "http://$domain/"
+  run curl_test -4 "https://$domain/"
+  run curl_test -6 "https://$domain/"
+  run curl -I -4 "https://$domain/"
+  run curl -I -4 "https://$domain/"
+  run curl -I -4 "https://$domain/"
+  run curl -I -6 "https://$domain/"
+  run curl -I -6 "https://$domain/"
+  run curl -I -6 "https://$domain/"
+) | tee /dev/stderr | ix
