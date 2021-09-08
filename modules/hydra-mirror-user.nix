cc27094cd8d30e2c672e33a8bdcc6766221c4430
Eelco Dolstra <edolstra@gmail.com>
2021-09-08 10:56:47 +0200

    hydra-mirror-user: remove isNormalUser
    Something is setting isSystemUser=true and both values are mutually exclusive.

--- modules/hydra-mirror-user.nix
+++ modules/hydra-mirror-user.nix
@@ -2,7 +2,6 @@
   users.users.hydra-mirror =
     { description = "Channel mirroring user";
       home = "/home/hydra-mirror";
-      isNormalUser = true;
       openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [ eelco rob ];
       uid = 497;
     };
