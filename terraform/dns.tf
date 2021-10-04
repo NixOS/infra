81f3eea779d2ed7687878e9fce0315a71ad7ff16
zimbatm <zimbatm@zimbatm.com>
2021-10-04 14:55:23 +0200

    terraform: add all CNAME records

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -75,6 +75,81 @@ locals {
       type     = "AAAA"
       value    = "2a01:4f8:c0c:6e2c::1"
     },
+    {
+      hostname = "_293364b7f7ebb076ac287cd132f8b316.cache.ngi0.nixos.org"
+      type     = "CNAME"
+      value    = "_6a75cfb0c20f4eaac96b72afaffb489b.auiqqraehs.acm-validations.aws"
+    },
+    {
+      hostname = "_acme-challenge.channels.nixos.org"
+      type     = "CNAME"
+      value    = "9u55qij5w2odiwqxfi.fastly-validations.com"
+    },
+    {
+      hostname = "_acme-challenge.releases.nixos.org"
+      type     = "CNAME"
+      value    = "s731ezp9ameh5f349b.fastly-validations.com"
+    },
+    {
+      hostname = "_acme-challenge.tarballs.nixos.org"
+      type     = "CNAME"
+      value    = "vnqm62k5sjx9jogeqg.fastly-validations.com"
+    },
+    {
+      hostname = "cache.ngi0.nixos.org"
+      type     = "CNAME"
+      value    = "d2tu257wv37zz1.cloudfront.net"
+    },
+    {
+      hostname = "cache.nixos.org"
+      type     = "CNAME"
+      value    = "dualstack.v2.shared.global.fastly.net"
+    },
+    {
+      hostname = "channels.nixos.org"
+      type     = "CNAME"
+      value    = "dualstack.v2.shared.global.fastly.net"
+    },
+    {
+      hostname = "chat.nixos.org"
+      type     = "CNAME"
+      value    = "nixos.element.io."
+    },
+    {
+      hostname = "conf.nixos.org"
+      type     = "CNAME"
+      value    = "nixconberlin.github.io"
+    },
+    {
+      hostname = "discourse.nixos.org"
+      type     = "CNAME"
+      value    = "nixos1.hosted-by-discourse.com"
+    },
+    {
+      hostname = "mobile.nixos.org"
+      type     = "CNAME"
+      value    = "nixos.github.io"
+    },
+    {
+      hostname = "planet.nixos.org"
+      type     = "CNAME"
+      value    = "nixos-planet.netlify.com"
+    },
+    {
+      hostname = "releases.nixos.org"
+      type     = "CNAME"
+      value    = "dualstack.v2.shared.global.fastly.net"
+    },
+    {
+      hostname = "tarballs.nixos.org"
+      type     = "CNAME"
+      value    = "dualstack.v2.shared.global.fastly.net"
+    },
+    {
+      hostname = "weekly.nixos.org"
+      type     = "CNAME"
+      value    = "nixos-weekly.netlify.com"
+    },
   ]
 }
 
