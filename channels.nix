f130648a4faa6b5868c10515a114e98572685541
Cole Helbling <cole.helbling@determinate.systems>
2021-11-23 12:29:43 -0800

    channels: format

--- channels.nix
+++ channels.nix
@@ -1,87 +1,87 @@
 rec {
   channels = {
-      # "Channel name" = {
-      #   job = "project/jobset/jobname"; like https://hydra.nixos.org/job/<value>/latest-finished
-      #   # when adding a new version, mark the oldest as "not current". New releases should always be current.
-      #   # Channels where `current = false` are marked `end-of-life` on https://status.nixos.org/ and alerting is disabled.
-      #   current = true;
-      # };
-      "nixos-unstable" = {
-        job = "nixos/trunk-combined/tested";
-        current = true;
-      };
-      "nixos-unstable-small" = {
-        job = "nixos/unstable-small/tested";
-        current = true;
-      };
-      "nixpkgs-unstable" = {
-        job = "nixpkgs/trunk/unstable";
-        current = true;
-      };
-      
-      "nixos-21.11" = {
-        job = "nixos/release-21.11/tested";
-        current = true;
-      };
-      "nixos-21.11-small" = {
-        job = "nixos/release-21.11-small/tested";
-        current = true;
-      };
-      "nixpkgs-21.11-darwin" = {
-        job = "nixpkgs/nixpkgs-21.11-darwin/darwin-tested";
-        current = true;
-      };
-      "nixos-21.11-aarch64" = {
-        job = "nixos/release-21.11-aarch64/tested";
-        current = true;
-      };
+    # "Channel name" = {
+    #   job = "project/jobset/jobname"; like https://hydra.nixos.org/job/<value>/latest-finished
+    #   # when adding a new version, mark the oldest as "not current". New releases should always be current.
+    #   # Channels where `current = false` are marked `end-of-life` on https://status.nixos.org/ and alerting is disabled.
+    #   current = true;
+    # };
+    "nixos-unstable" = {
+      job = "nixos/trunk-combined/tested";
+      current = true;
+    };
+    "nixos-unstable-small" = {
+      job = "nixos/unstable-small/tested";
+      current = true;
+    };
+    "nixpkgs-unstable" = {
+      job = "nixpkgs/trunk/unstable";
+      current = true;
+    };
 
-      "nixos-21.05" = {
-        job = "nixos/release-21.05/tested";
-        current = true;
-      };
-      "nixos-21.05-small" = {
-        job = "nixos/release-21.05-small/tested";
-        current = true;
-      };
-      "nixpkgs-21.05-darwin" = {
-        job = "nixpkgs/nixpkgs-21.05-darwin/darwin-tested";
-        current = true;
-      };
-      "nixos-21.05-aarch64" = {
-        job = "nixos/release-21.05-aarch64/tested";
-        current = true;
-      };
+    "nixos-21.11" = {
+      job = "nixos/release-21.11/tested";
+      current = true;
+    };
+    "nixos-21.11-small" = {
+      job = "nixos/release-21.11-small/tested";
+      current = true;
+    };
+    "nixpkgs-21.11-darwin" = {
+      job = "nixpkgs/nixpkgs-21.11-darwin/darwin-tested";
+      current = true;
+    };
+    "nixos-21.11-aarch64" = {
+      job = "nixos/release-21.11-aarch64/tested";
+      current = true;
+    };
 
-      "nixos-20.09" = {
-        job = "nixos/release-20.09/tested";
-        current = false;
-      };
-      "nixos-20.09-small" = {
-        job = "nixos/release-20.09-small/tested";
-        current = false;
-      };
-      "nixpkgs-20.09-darwin" = {
-        job = "nixpkgs/nixpkgs-20.09-darwin/darwin-tested";
-        current = false;
-      };
-      "nixos-20.09-aarch64" = {
-        job = "nixos/release-20.09-aarch64/tested";
-        current = false;
-      };
+    "nixos-21.05" = {
+      job = "nixos/release-21.05/tested";
+      current = true;
+    };
+    "nixos-21.05-small" = {
+      job = "nixos/release-21.05-small/tested";
+      current = true;
+    };
+    "nixpkgs-21.05-darwin" = {
+      job = "nixpkgs/nixpkgs-21.05-darwin/darwin-tested";
+      current = true;
+    };
+    "nixos-21.05-aarch64" = {
+      job = "nixos/release-21.05-aarch64/tested";
+      current = true;
+    };
 
-      "nixos-20.03" = {
-        job = "nixos/release-20.03/tested";
-        current = false;
-      };
-      "nixos-20.03-small" = {
-        job = "nixos/release-20.03-small/tested";
-        current = false;
-      };
-      "nixpkgs-20.03-darwin" = {
-        job = "nixpkgs/nixpkgs-20.03-darwin/darwin-tested";
-        current = false;
-      };
+    "nixos-20.09" = {
+      job = "nixos/release-20.09/tested";
+      current = false;
+    };
+    "nixos-20.09-small" = {
+      job = "nixos/release-20.09-small/tested";
+      current = false;
+    };
+    "nixpkgs-20.09-darwin" = {
+      job = "nixpkgs/nixpkgs-20.09-darwin/darwin-tested";
+      current = false;
+    };
+    "nixos-20.09-aarch64" = {
+      job = "nixos/release-20.09-aarch64/tested";
+      current = false;
+    };
+
+    "nixos-20.03" = {
+      job = "nixos/release-20.03/tested";
+      current = false;
+    };
+    "nixos-20.03-small" = {
+      job = "nixos/release-20.03-small/tested";
+      current = false;
+    };
+    "nixpkgs-20.03-darwin" = {
+      job = "nixpkgs/nixpkgs-20.03-darwin/darwin-tested";
+      current = false;
+    };
   };
 
   channels-with-urls = (builtins.mapAttrs (name: about: about.job) channels);
