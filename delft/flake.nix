f313baa9f62f0d241619857ac6a552f9952fe35f
Graham Christensen <graham@grahamc.com>
2021-12-18 21:50:31 +0100

    delft/flake.lock: Update to 21.11
    Flake lock file changes:
    
    • Updated input 'hydra':
        'github:NixOS/hydra/cf6f78c365d6c8b65f38711d9628ee6bfb86f361' (2021-12-15)
      → 'github:NixOS/hydra/e1e5fafdff63c1e1595d2edb8c9854710211a0d7' (2021-12-17)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/c7e905b6a971dad2c26a31960053176c770a812a' (2021-04-16)
      → 'github:NixOS/nixpkgs/76fdcfe47c9bf88a941b47e224e537c516c8d7ff' (2021-12-18)

--- delft/flake.nix
+++ delft/flake.nix
@@ -1,6 +1,6 @@
 {
   #inputs.nixpkgs.url = "/home/deploy/src/edolstra-nixpkgs"; # toString ../../edolstra-nixpkgs; # = "nixpkgs/release-19.09";
-  inputs.nixpkgs.url = "nixpkgs/nixos-20.09-small";
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.11-small";
   inputs.nix.follows = "hydra/nix";
 
   outputs = flakes @ { self, nixpkgs, nix, hydra, dwarffs }: {
