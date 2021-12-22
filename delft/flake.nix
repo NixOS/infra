bb31eab9cc48bf388057de96db7170d24ae6631f
Eelco Dolstra <edolstra@gmail.com>
2021-12-22 02:01:15 +0100

    Switch back to hydra's master branch to record timing information before #1083

--- delft/flake.nix
+++ delft/flake.nix
@@ -2,7 +2,7 @@
   #inputs.nixpkgs.url = "/home/deploy/src/edolstra-nixpkgs"; # toString ../../edolstra-nixpkgs; # = "nixpkgs/release-19.09";
   inputs.nixpkgs.url = "nixpkgs/nixos-21.11-small";
   inputs.nix.follows = "hydra/nix";
-  inputs.hydra.url = "github:DeterminateSystems/hydra?ref=faster-notifications";
+  #inputs.hydra.url = "github:DeterminateSystems/hydra?ref=faster-notifications";
 
   outputs = flakes @ { self, nixpkgs, nix, hydra, dwarffs }: {
 
