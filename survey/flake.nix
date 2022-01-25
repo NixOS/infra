4aa7bdda45ae6a4a3e8a4addb60ce7c974056eb0
Eelco Dolstra <edolstra@gmail.com>
2022-01-25 17:33:39 +0100

    survey: Add initial config

--- survey/flake.nix
+++ survey/flake.nix
@@ -1,10 +1,31 @@
+# To bootstrap:
+#   $ nix-shell -p nixUnstable git --run "nix build --experimental-features 'nix-command flakes' ~/nixos-org-configurations/survey#nixosConfigurations.survey.config.system.build.toplevel"
+#   $ ./result/bin/switch-to-configuration test
+#
+# To update:
+#   $ nixos-rebuild switch --flake ~/nixos-org-configurations/survey
+
 {
   inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
 
   outputs = flakes @ { self, nixpkgs }: {
     nixosConfigurations.survey = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ (import ./configuration.nix flakes) ];
+      modules =
+        [
+          "${nixpkgs}/nixos/modules/virtualisation/amazon-image.nix"
+          ../modules/common.nix
+          ({ config, pkgs, ... }:
+          {
+            ec2.hvm = true;
+            networking.hostName = "survey";
+            networking.firewall.allowedTCPPorts = [ 80 443 ];
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+            nix.package = pkgs.nixUnstable;
+            nix.registry.nixpkgs.flake = nixpkgs;
+            users.users.root.openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [ eelco garbas ];
+          })
+        ];
     };
 
     devShell.x86_64-linux =
