bc079538b7f18d93788f6675fabd9e8edd7bc8ab
Eelco Dolstra <edolstra@gmail.com>
2022-02-19 14:39:44 +0100

    Go back to hydra main

--- delft/flake.nix
+++ delft/flake.nix
@@ -2,7 +2,7 @@
   #inputs.nixpkgs.url = "/home/deploy/src/edolstra-nixpkgs"; # toString ../../edolstra-nixpkgs; # = "nixpkgs/release-19.09";
   inputs.nixpkgs.url = "nixpkgs/nixos-21.11-small";
   inputs.nix.follows = "hydra/nix";
-  inputs.hydra.url = "github:NixOS/hydra/nix-2.6-rebase"; # Testing Hydra PR #1140, updating Nix to 2.6
+  inputs.hydra.url = "github:NixOS/hydra/master";
   outputs = flakes @ { self, nixpkgs, nix, hydra /*, dwarffs */ }: {
 
     nixopsConfigurations.default =
