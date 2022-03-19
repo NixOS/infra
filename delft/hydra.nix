64dab2175a08cf9ef93d83fb3901010c9f6f2239
Eelco Dolstra <edolstra@gmail.com>
2022-03-19 01:28:12 +0100

    PRioritize certs

--- delft/hydra.nix
+++ delft/hydra.nix
@@ -78,7 +78,7 @@ in
   systemd.services.hydra-queue-runner.wantedBy = mkForce [];
   systemd.services.hydra-queue-runner.requires = mkForce [];
 
-  programs.ssh.hostKeyAlgorithms = [ "ssh-ed25519" "ssh-rsa" "ecdsa-sha2-nistp256" "rsa-sha2-512-cert-v01@openssh.com" ];
+  programs.ssh.hostKeyAlgorithms = [ "rsa-sha2-512-cert-v01@openssh.com" "ssh-ed25519" "ssh-rsa" "ecdsa-sha2-nistp256"];
   programs.ssh.extraConfig = mkAfter
     ''
       ServerAliveInterval 120
