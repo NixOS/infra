7b0a8ce363318b3c6246a0a7d9407a0f86621b9c
Graham Christensen <graham@grahamc.com>
2022-03-21 14:45:52 +0100

    Go back to hydra master

--- delft/flake.nix
+++ delft/flake.nix
@@ -2,7 +2,7 @@
   #inputs.nixpkgs.url = "/home/deploy/src/edolstra-nixpkgs"; # toString ../../edolstra-nixpkgs; # = "nixpkgs/release-19.09";
   inputs.nixpkgs.url = "nixpkgs/nixos-21.11-small";
   inputs.nix.follows = "hydra/nix";
-  inputs.hydra.url = "github:DeterminateSystems/hydra/hydra-update-gc-roots/network-traffic"; # "github:NixOS/hydra/master";
+  inputs.hydra.url = "github:NixOS/hydra/master";
   outputs = flakes @ { self, nixpkgs, nix, hydra /*, dwarffs */ }: {
 
     nixopsConfigurations.default =
