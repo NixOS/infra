77ea3da5134ff51d5439c8989f52e46c702ec58a
Graham Christensen <graham@grahamc.com>
2022-03-31 19:16:33 +0200

    eris: enable nginx services.nginx.recommendedProxySettings

--- delft/eris.nix
+++ delft/eris.nix
@@ -41,6 +41,7 @@ in {
 
   services.nginx = {
     enable = true;
+    recommendedProxySettings = true;
     virtualHosts."monitoring.nixos.org" = {
       enableACME = true;
       forceSSL = true;
