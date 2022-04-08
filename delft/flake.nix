a0e6ac98d0ccce5c8ce50a29faa7473532ca743f
Graham Christensen <graham@grahamc.com>
2022-04-08 20:30:07 +0200

    hydra: deploy an updated queue runner exporter

--- delft/flake.nix
+++ delft/flake.nix
@@ -2,7 +2,8 @@
   #inputs.nixpkgs.url = "/home/deploy/src/edolstra-nixpkgs"; # toString ../../edolstra-nixpkgs; # = "nixpkgs/release-19.09";
   inputs.nixpkgs.url = "nixpkgs/nixos-21.11-small";
   inputs.nix.follows = "hydra/nix";
-  inputs.hydra.url = "github:NixOS/hydra/master";
+  #inputs.hydra.url = "github:NixOS/hydra/master";
+  inputs.hydra.url = "github:DeterminateSystems/hydra/queue-runner-exporter";
   outputs = flakes @ { self, nixpkgs, nix, hydra /*, dwarffs */ }: {
 
     nixopsConfigurations.default =
