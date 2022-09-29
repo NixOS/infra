8b262e7417d7f594d44c86da605ac8e90c7cd431
Eelco Dolstra <edolstra@gmail.com>
2022-09-30 01:32:01 +0200

    fixup flake

--- delft/flake.nix
+++ delft/flake.nix
@@ -7,8 +7,7 @@
   inputs.hydra.inputs.nix.url = "github:edolstra/nix/hydra-temp";
   inputs.nix-netboot-serve.url = "github:DeterminateSystems/nix-netboot-serve";
   #inputs.hydra.url = "github:DeterminateSystems/hydra/queue-runner-exporter";
-  outputs = flakes @ { self, nixpkgs, nix, hydra /*, dwarffs */ }: {
-
+  outputs = flakes @ { self, nixpkgs, nix, hydra, nix-netboot-serve /*, dwarffs */ }: {
     nixopsConfigurations.default =
       { inherit nixpkgs; }
       // import ./network.nix flakes;
