b5cff252ad16304cc34431bf039845035f3425a9
Graham Christensen <graham@grahamc.com>
2022-09-29 20:59:37 -0400

    make the monioring page default on eris

--- delft/eris.nix
+++ delft/eris.nix
@@ -79,6 +79,7 @@ in
     virtualHosts."monitoring.nixos.org" = {
       enableACME = true;
       forceSSL = true;
+      default = true;
       locations."/".return = "302 https://status.nixos.org";
       locations."/prometheus/".proxyPass = "http://${config.services.prometheus.listenAddress}:${toString config.services.prometheus.port}";
       locations."/grafana/".proxyPass = "http://${config.services.grafana.addr}:${toString config.services.grafana.port}/";
