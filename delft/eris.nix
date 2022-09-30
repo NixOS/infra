da64bed10cd666e61697efca84a75ea4d0854ea3
Graham Christensen <graham@grahamc.com>
2022-09-29 20:18:37 -0400

    turn on tls for netboot

--- delft/eris.nix
+++ delft/eris.nix
@@ -40,6 +40,29 @@ in
     listen = "127.0.0.1:3000";
   };
 
+  security.acme = {
+    # these cert parameters are very specifically & carefully chosen for iPXE compatibility.
+    certs."netboot.nixos.org" = {
+      keyType = "rsa4096";
+      extraLegoRunFlags = [
+        # re: https://community.letsencrypt.org/t/production-chain-changes/150739/1
+        # re: https://github.com/ipxe/ipxe/pull/116
+        # re: https://github.com/ipxe/ipxe/pull/112
+        # re: https://lists.ipxe.org/pipermail/ipxe-devel/2020-May/007042.html
+        "--preferred-chain"
+        "ISRG Root X1"
+      ];
+      extraLegoRenewFlags = [
+        # re: https://community.letsencrypt.org/t/production-chain-changes/150739/1
+        # re: https://github.com/ipxe/ipxe/pull/116
+        # re: https://github.com/ipxe/ipxe/pull/112
+        # re: https://lists.ipxe.org/pipermail/ipxe-devel/2020-May/007042.html
+        "--preferred-chain"
+        "ISRG Root X1"
+      ];
+    };
+  };
+
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
@@ -48,8 +71,8 @@ in
     sslCiphers = options.services.nginx.sslCiphers.default + ":AES256-SHA256"; # iPXE needs AES256-SHA256
 
     virtualHosts."netboot.nixos.org" = {
-      enableACME = false;
-      forceSSL = false;
+      enableACME = true;
+      forceSSL = true;
       locations."/".proxyPass = "http://127.0.0.1:3000/";
     };
 
