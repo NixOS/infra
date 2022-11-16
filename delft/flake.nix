3e4a503f3765ec80945a44d36136a3d2d705668a
Eelco Dolstra <edolstra@gmail.com>
2022-11-16 17:06:45 +0100

    Use lazy-trees nix/hydra

--- delft/flake.nix
+++ delft/flake.nix
@@ -2,9 +2,8 @@
   #inputs.nixpkgs.url = "/home/deploy/src/edolstra-nixpkgs"; # toString ../../edolstra-nixpkgs; # = "nixpkgs/release-19.09";
   inputs.nixpkgs.url = "nixpkgs/nixos-22.05-small";
   inputs.nix.follows = "hydra/nix";
-  inputs.hydra.url = "github:NixOS/hydra";
+  inputs.hydra.url = "github:NixOS/hydra/lazy-trees";
   inputs.nix-netboot-serve.url = "github:DeterminateSystems/nix-netboot-serve";
-  inputs.hydra.inputs.nix.url = "github:edolstra/nix/hydra-temp";
   #inputs.hydra.url = "github:DeterminateSystems/hydra/queue-runner-exporter";
   outputs = flakes @ { self, nixpkgs, nix, hydra, nix-netboot-serve /*, dwarffs */ }: {
     nixopsConfigurations.default =
