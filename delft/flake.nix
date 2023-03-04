a077c9d3958b12c2730d4950447852baefdf5280
Linus Heckemann <git@sphalerite.org>
2023-03-04 02:09:23 +0100

    Hack together a nixosConfigurations output
    While the resulting configurations aren't buildable (missing settings
    from nixops state for fileSystems for example), it's enough to check
    settings using commands like:
    
    nix eval .#nixosConfigurations.eris.config.deployment.keys --json | jq

--- delft/flake.nix
+++ delft/flake.nix
@@ -13,5 +13,20 @@
       { inherit nixpkgs; }
       // import ./network.nix flakes;
 
+    nixosConfigurations = builtins.removeAttrs (lib.mapAttrs (name: value: nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        value
+        self.nixopsConfigurations.default.defaults
+        {
+          # hack: this doesn't check deployment options properly, but
+          # is enough to have the deployment options ignored and allow
+          # evaluating individual config options.
+          options.deployment = lib.mkOption {
+            type = (nixpkgs.legacyPackages.x86_64-linux.formats.json {}).type;
+          };
+        }
+      ];
+    }) self.nixopsConfigurations.default) ["defaults"];
   };
 }
