3766ae4743dd0d5714abb3b68b42556502501e54
Linus Heckemann <git@sphalerite.org>
2023-04-03 18:20:22 +0200

    Use 22.11

--- delft/flake.nix
+++ delft/flake.nix
@@ -1,6 +1,6 @@
 {
   #inputs.nixpkgs.url = "/home/deploy/src/edolstra-nixpkgs"; # toString ../../edolstra-nixpkgs; # = "nixpkgs/release-19.09";
-  inputs.nixpkgs.url = "nixpkgs/nixos-22.05-small";
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.11-small";
   inputs.nix.follows = "hydra/nix";
   inputs.hydra.url = "github:NixOS/hydra/lazy-trees";
   inputs.nix-netboot-serve.url = "github:DeterminateSystems/nix-netboot-serve";
