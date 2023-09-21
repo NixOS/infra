b28bec67e339987ad26c3daf48f898834b6967c4
Cole Helbling <cole.helbling@determinate.systems>
2023-09-21 07:59:16 -0700

    rhea: add a backup arm64 bid

--- delft/hydra-scaler.nix
+++ delft/hydra-scaler.nix
@@ -38,6 +38,27 @@
               }
             ];
           };
+          # Try to ensure we have at least 1 aarch64-linux builder by bidding slightly higher than
+          # usual (the minimum is *intentionally* 0 so that we don't hold onto this "backup" machine
+          # once we go below 5000 runnable builds). The goal is that this bid will only go through
+          # when we have at least 5000 runnable builds for aarch64-linux, and hopefully prevents us
+          # from having 0 aarch64 machines if the spot market prices rise too high.
+          # Spot market prices for c3.large.arm64 over the last week:
+          # https://monitoring.nixos.org/grafana/d/ItOJVUoWk/packet-spot-prices?var-plan=c3.large.arm64&from=now-7d&to=now&orgId=1&refresh=10s
+          # Spot market capacity for c3.large.arm64 over the last 5 minutes:
+          # https://monitoring.nixos.org/grafana/d/I1WQEbbWz/packet-capacity-by-plan-table?orgId=1&var-plan=c3.large.arm64&var-facility=All
+          backup = {
+            divisor = 5000;
+            minimum = 0; # *intentionally* 0 so we don't hold onto this machine forever and skew the spot market prices too high
+            maximum = 1;
+            plans = [
+              {
+                bid = 2.5;
+                plan = "c3.large.arm64";
+                netboot_url = "${netboot_base}/c3-large-arm";
+              }
+            ];
+          };
         };
         x86_64-linux = rec {
           bigparallel = {
