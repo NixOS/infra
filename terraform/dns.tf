c812d7388a7ae083a0c5044f22b3948cdf3ca1ed
Pierre Bourdon <delroth@gmail.com>
2023-10-25 16:24:31 +0200

    dns, fastly: add cache-staging.nixos.org domain + TLS subscription

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -107,6 +107,11 @@ locals {
       value    = "dualstack.v2.shared.global.fastly.net"
     },
     {
+      hostname = "cache-staging.nixos.org"
+      type     = "CNAME"
+      value    = "dualstack.v2.shared.global.fastly.net"
+    },
+    {
       hostname = "channels.nixos.org"
       type     = "CNAME"
       value    = "dualstack.v2.shared.global.fastly.net"
