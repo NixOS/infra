71ec8a02ea7cb86377de939484394b80f4efe4cd
Pierre Bourdon <delroth@gmail.com>
2023-10-27 16:31:51 +0200

    dns, fastly: add cache-staging.nixos.org domain + TLS subscription (#294)

--- terraform/cache.tf
+++ terraform/cache.tf
@@ -253,3 +253,9 @@ resource "fastly_tls_subscription" "cache" {
   configuration_id      = local.fastly_tls12_sni_configuration_id
   certificate_authority = "globalsign"
 }
+
+resource "fastly_tls_subscription" "cache-staging" {
+  domains               = ["cache-staging.nixos.org"]
+  configuration_id      = local.fastly_tls12_sni_configuration_id
+  certificate_authority = "globalsign"
+}
