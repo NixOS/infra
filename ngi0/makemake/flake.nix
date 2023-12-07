6a8e1ef26d44f89323ed396ead79b89e8c5e2ec0
John Ericson <git@JohnEricson.me>
2023-12-06 20:03:20 -0500

    makemake: Stage Hydra PR 1316 (#316)
    That is https://github.com/NixOS/hydra/pull/1316, preparation for
    broader CA derivations support in a way that does not require scheme
    changes.

--- ngi0/makemake/flake.nix
+++ ngi0/makemake/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.follows = "hydra/nixpkgs";
   inputs.nix.follows = "hydra/nix";
-  inputs.hydra.url = "github:NixOS/hydra";
+  inputs.hydra.url = "github:NixOS/hydra/ca-derivations-prep";
 
   outputs = { self, nixpkgs, nix, hydra }: {
 
