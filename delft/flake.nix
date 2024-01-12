a078fbf608c930828afb27b97de9dbc92a6146b8
Pierre Bourdon <delroth@gmail.com>
2024-01-12 03:25:10 +0100

    delft: flake: add nixosConfigurations outputs for the core machines (currently eris doesn't work due to more nixops dependencies)

--- delft/flake.nix
+++ delft/flake.nix
@@ -5,26 +5,25 @@
   outputs = flakes @ { self, nixpkgs, nix-netboot-serve }:
     let inherit (nixpkgs) lib;
   in {
+    nixosConfigurations.eris = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./eris.nix
+        flakes.nix-netboot-serve.nixosModules.nix-netboot-serve
+      ];
+    };
+
+    nixosConfigurations.haumea = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./haumea.nix
+      ];
+    };
+
     nixopsConfigurations.default =
       { inherit nixpkgs; }
       // import ./network.nix flakes;
-
-    /*
-    nixosConfigurations = builtins.removeAttrs (lib.mapAttrs (name: value: nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        value
-        self.nixopsConfigurations.default.defaults
-        {
-          # hack: this doesn't check deployment options properly, but
-          # is enough to have the deployment options ignored and allow
-          # evaluating individual config options.
-          options.deployment = lib.mkOption {
-            type = (nixpkgs.legacyPackages.x86_64-linux.formats.json {}).type;
-          };
-        }
-      ];
-    }) self.nixopsConfigurations.default) ["defaults"];
-    */
   };
 }
