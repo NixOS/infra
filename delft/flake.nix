96ed312aed301f35b7bb161ecf19849c08bb3531
Pierre Bourdon <delroth@gmail.com>
2024-01-27 23:14:02 +0100

    delft: add agenix to flake inputs

--- delft/flake.nix
+++ delft/flake.nix
@@ -2,7 +2,12 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-23.05-small";
   inputs.nix-netboot-serve.url = "github:DeterminateSystems/nix-netboot-serve";
 
-  outputs = flakes @ { self, nixpkgs, nix-netboot-serve }:
+  inputs.agenix = {
+    url = "github:ryantm/agenix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = flakes @ { self, agenix, nixpkgs, nix-netboot-serve }:
     let inherit (nixpkgs) lib;
   in {
     nixosConfigurations.eris = nixpkgs.lib.nixosSystem {
@@ -11,7 +16,8 @@
       modules = [
         ./eris.nix
         ./eris-physical.nix
-        flakes.nix-netboot-serve.nixosModules.nix-netboot-serve
+        agenix.nixosModules.age
+        nix-netboot-serve.nixosModules.nix-netboot-serve
       ];
     };
 
@@ -27,5 +33,14 @@
     nixopsConfigurations.default =
       { inherit nixpkgs; }
       // import ./network.nix flakes;
+
+    # TODO: flake-utils.lib.eachDefaultSystem
+    devShell.x86_64-linux = let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in pkgs.mkShell {
+      buildInputs = with pkgs; [
+        agenix.packages.x86_64-linux.agenix
+      ];
+    };
   };
 }
