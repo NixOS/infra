985243448b2d886f8a4fba48ce2a2240bfc0c3e6
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-02-03 19:47:32 +0100

    prometheus-nixos-exporter: reformat
    Reformat the file using `ruff format`.

--- modules/prometheus/nixos-exporter.py
+++ modules/prometheus/nixos-exporter.py
@@ -33,24 +33,29 @@ class NixosSystemCollector:
         current_system = GaugeMetricFamily(
             "nixos_current_system_time_seconds",
             "The time the system's current generation was registered in the Nix database.",
-            labels=["version_id"]
+            labels=["version_id"],
         )
-        current_system.add_metric([self.get_version_id("/run/current-system")],
-                                  self.get_time("/run/current-system"))
+        current_system.add_metric(
+            [self.get_version_id("/run/current-system")],
+            self.get_time("/run/current-system"),
+        )
         yield current_system
 
         booted_system = GaugeMetricFamily(
             "nixos_booted_system_time_seconds",
             "The time the system's booted generation was registered in the Nix database.",
-            labels=["version_id"]
+            labels=["version_id"],
         )
-        booted_system.add_metric([self.get_version_id("/run/booted-system")], self.get_time("/run/booted-system"))
+        booted_system.add_metric(
+            [self.get_version_id("/run/booted-system")],
+            self.get_time("/run/booted-system"),
+        )
         yield booted_system
 
     def get_version_id(self, path):
         result = subprocess.run(
-            [ "bash", "-c", f"source {path}/etc/os-release; echo $VERSION_ID" ],
-            stdout=subprocess.PIPE
+            ["bash", "-c", f"source {path}/etc/os-release; echo $VERSION_ID"],
+            stdout=subprocess.PIPE,
         )
         if result.returncode == 0:
             return result.stdout.decode("utf-8").strip()
@@ -74,13 +79,13 @@ class NixosSystemCollector:
 
         return 0
 
+
 registry = CollectorRegistry()
 registry.register(NixosSystemCollector())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Start up the server to expose the metrics.
     start_http_server(9300, registry=registry)
 
     while True:
         time.sleep(100000)
-
