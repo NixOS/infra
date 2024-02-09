3bfe2842337f1ecd1e4d3304ba678ac2aed925c3
Pierre Bourdon <delroth@gmail.com>
2024-02-09 23:46:58 +0100

    delft: merge back rhea to the main flake
    There's now only one flake and one nixpkgs version for all 3 main core
    NixOS infra machines.

--- delft/flake.nix
+++ delft/flake.nix
@@ -1,26 +1,33 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11-small";
+
+  inputs.agenix.url = "github:ryantm/agenix";
+  inputs.agenix.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.hydra.url = "github:NixOS/hydra/nix-2.19";
+  inputs.nix.follows = "hydra/nix";
+
+  inputs.hydra-scale-equinix-metal.url = "github:DeterminateSystems/hydra-scale-equinix-metal";
+
   inputs.nix-netboot-serve.url = "github:DeterminateSystems/nix-netboot-serve";
 
   inputs.rfc39.url = "github:NixOS/rfc39";
   inputs.rfc39.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.agenix = {
-    url = "github:ryantm/agenix";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = flakes @ { self, agenix, nixpkgs, nix-netboot-serve, rfc39 }:
+  outputs = flakes @ { self, agenix, hydra, hydra-scale-equinix-metal, nix, nixpkgs, nix-netboot-serve, rfc39 }:
     let
       inherit (nixpkgs) lib;
 
       flakesModule = {
         imports = [
           agenix.nixosModules.age
+          hydra.nixosModules.hydra
+          hydra-scale-equinix-metal.nixosModules.default
           nix-netboot-serve.nixosModules.nix-netboot-serve
         ];
 
         nixpkgs.overlays = [
+          nix.overlays.default
           rfc39.overlays.default
         ];
       };
@@ -42,6 +49,15 @@
           flakesModule
           ./haumea.nix
           ./haumea-physical.nix
+        ];
+      };
+
+      nixosConfigurations.rhea = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          flakesModule
+          ./rhea/configuration.nix
         ];
       };
 
