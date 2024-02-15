7e034abd1a3f585e9b2de8ddd58397c40db1fb45
Vladimír Čunát <v@cunat.cz>
2024-02-15 11:34:16 +0100

    rhea: more zramSwap
    We still commonly get evaluation failures that look like
    caused by lack of memory.  Nixpkgs grows all the time, too.
    Anyway, I feel relatively confident about increasing this to 80%.

--- delft/rhea/configuration.nix
+++ delft/rhea/configuration.nix
@@ -36,6 +36,7 @@
 
   # hydra-evaluator causes very sharp spikes in RAM usage on trunk-combined
   zramSwap.enable = true;
+  zramSwap.memoryPercent = 80;
 
   nix.gc.automatic = true;
   nix.gc.options = ''--max-freed "$((400 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
