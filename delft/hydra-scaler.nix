20a8da6bc35c32f9fa449ffb59b717794175a857
Cole Helbling <cole.e.helbling@outlook.com>
2024-02-17 12:18:54 -0800

    hydra-scaler: stop autoscaling arm64

--- delft/hydra-scaler.nix
+++ delft/hydra-scaler.nix
@@ -13,32 +13,17 @@
       facilities = ["any"];
       tags = ["hydra"];
       categories = {
-        aarch64-linux = {
-          bigparallel = {
-            divisor = 24;
-            minimum = 1;
-            maximum = 3;
-            plans = [
-              {
-                bid = 2.0;
-                plan = "c3.large.arm64";
-                netboot_url = "${netboot_base}/c3-large-arm--big-parallel";
-              }
-            ];
-          };
-          small = {
-            divisor = 4000;
-            minimum = 0; # we have one static 80-core in addition
-            maximum = 3;
-            plans = [
-              {
-                bid = 2.0;
-                plan = "c3.large.arm64";
-                netboot_url = "${netboot_base}/c3-large-arm";
-              }
-            ];
-          };
-        };
+        # NOTE(cole-h): We don't autoscale arm64 anymore because EM asked us not to: the arm64 spot
+        # market appears to be a little funky as of this comment (we would commonly spin up a
+        # machine, only for it to be reclaimed before it was even able to boot into NixOS and run
+        # even 1 build for Hydra).
+        # As of 17 Feb 2024, we have 2 dedicated arm64 machines on EM -- one `small`
+        # (`small-c3.large.arm64`) and one `big-parallel` (`big-parallel-c3.large.arm64`). Hopefully
+        # this will be an improvement over "maybe we have no arm64 machines at all because they spin
+        # down before they can do any work".
+        # The netboot URL for arm64 big-parallel is: https://netboot.nixos.org/dispatch/hydra/hydra.nixos.org/equinix-metal-builders/main/c3-large-arm--big-parallel ("Always PXE" enabled, "hydra" tag)
+        # The netboot URL for arm64 small is: https://netboot.nixos.org/dispatch/hydra/hydra.nixos.org/equinix-metal-builders/main/c3-large-arm ("Always PXE" enabled, "hydra" tag)
+
         x86_64-linux = rec {
           bigparallel = {
             divisor = 16;
