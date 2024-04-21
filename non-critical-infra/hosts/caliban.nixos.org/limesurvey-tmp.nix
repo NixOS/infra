67b1f9a08dd9ad3aa83adfc4b8a7862eecdca0bb
Julien Malka <julien@malka.sh>
2024-04-21 18:32:02 +0200

    init survey on caliban

---
+++ non-critical-infra/hosts/caliban.nixos.org/limesurvey-tmp.nix
@@ -0,0 +1,31 @@
+# the content of this file should be put in the modules folder once the actual module has been upstreamed
+# PR: https://github.com/NixOS/nixpkgs/pull/325665/
+{ config, pkgs, ... }:
+{
+  disabledModules = [ "services/web-apps/limesurvey.nix" ];
+
+  imports = [ ../../modules/limesurvey.nix ];
+
+  services.limesurvey = {
+    enable = true;
+    encryptionKeyFile = config.sops.secrets.limesurvey-encryption-key.path;
+    encryptionNonceFile = config.sops.secrets.limesurvey-encryption-nonce.path;
+    virtualHost = {
+      serverName = "survey.nixos.org";
+      enableACME = true;
+      forceSSL = true;
+    };
+  };
+
+  sops.secrets.limesurvey-encryption-key = {
+    format = "binary";
+    sopsFile = ../../secrets/limesurvey-encryption-key.caliban;
+  };
+
+  sops.secrets.limesurvey-encryption-nonce = {
+    format = "binary";
+    sopsFile = ../../secrets/limesurvey-encryption-nonce.caliban;
+  };
+
+
+}
