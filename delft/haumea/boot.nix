37293ecb9e48e125b0837418711edbcaa7ffb1d0
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-05-23 21:09:59 +0200

    haumea: set configuration limit
    The /boot filesystem on haumea only has 230 MB capacity.
    
    With an initrd size of 25M and a bzImage of 8.6 this allows 6-7 kernels,
    and 10 generations should be fine due to some generations not containing
    any kernel bumps.

--- delft/haumea/boot.nix
+++ delft/haumea/boot.nix
@@ -5,6 +5,7 @@
       "/dev/nvme1n1"
     ];
     copyKernels = true;
+    configurationLimit = 10; # 230 MB /boot capacity
   };
   boot.initrd.availableKernelModules = [ "ahci" "nvme" "usbhid" ];
   boot.kernelModules = [ "kvm-amd" ];
