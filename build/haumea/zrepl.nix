5429e8890a02858ae02fd994ead51c34148658c2
Vladimír Čunát <v@cunat.cz>
2024-06-23 15:04:38 +0200

    haumea/zrepl WIP: reduce snapshot count
    We repeatedly run out of space on Haumea.

--- build/haumea/zrepl.nix
+++ build/haumea/zrepl.nix
@@ -22,7 +22,7 @@
       filesystems."rpool/safe<" = true;
       snapshotting = {
         type = "periodic";
-        interval = "15m";
+        interval = "1h";
         prefix = "zrepl_snap_";
         hooks = [ {
           # https://zrepl.github.io/master/configuration/snapshotting.html#postgres-checkpoint-hook
@@ -33,14 +33,23 @@
       };
       pruning = {
         keep_sender = [
+          { type = "not_replicated"; }
           {
             type = "grid";
             regex = "^zrepl_snap_.*";
             grid = lib.concatStringsSep " | " [
-              "4x15m"
-              "24x1h"
-              "4x1d"
-              "3x1w"
+              "1x1h"
+              "1x2h"
+              "1x4h"
+              # "grid" acts weird if an interval isn't a whole-number multiple
+              # of the previous one, so we jump from 8h to 24h
+              "2x8h"
+              "1x1d"
+              "1x2d"
+              "1x4d"
+              "1x8d"
+              # At this point we keep 9 snapshots spanning 8--16 days (depends on moment),
+              # with exponentially increasing spacing (almost).
             ];
           }
         ];
@@ -48,8 +57,10 @@
           { type = "grid";
             regex = "^zrepl_snap_.*";
             grid = lib.concatStringsSep " | " [
-              "96x1h"
-              "12x4h"
+              "1x1h"
+              "1x2h"
+              "1x4h"
+              "2x8h"
               "7x1d"
               "52x1w"
             ];
