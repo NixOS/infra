c30232e1953d057207c10b3671e0452bdf1087d1
Jörg Thalheim <joerg@thalheim.io>
2024-08-17 09:55:52 +0200

    add treefmt-nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Nixos org infra";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+  outputs = inputs@{ flake-parts, ... }:
+  flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    imports = [ ./formatter/default.nix ];
+  };
+
+}
---
+++ formatter/default.nix
@@ -0,0 +1,21 @@
+{ inputs, ... }: {
+  imports = [ inputs.treefmt-nix.flakeModule ];
+
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        # Used to find the project root
+        projectRootFile = ".git/config";
+
+        programs.terraform.enable = true;
+        programs.deadnix.enable = true;
+        programs.nixfmt.enable = true;
+        programs.nixfmt.package = pkgs.nixfmt-rfc-style;
+        programs.shellcheck.enable = true;
+
+        programs.shfmt.enable = true;
+        programs.rustfmt.enable = true;
+      };
+  };
+}
