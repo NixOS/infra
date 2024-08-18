da17de1c9107b77659d305a8260a6acf0ed92011
Jörg Thalheim <joerg@thalheim.io>
2024-08-18 12:52:36 +0200

    merge non-critical infra into toplevel flake
    This allows us to have a single place to update nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,41 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    colmena = {
+      url = "github:zhaofengli/colmena";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+        stable.follows = "nixpkgs";
+      };
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    srvos = {
+      url = "github:numtide/srvos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs";
+      };
+    };
+    first-time-contribution-tagger = {
+      url = "github:Janik-Haag/first-time-contribution-tagger";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
   outputs =
     inputs@{ flake-parts, ... }:
@@ -18,7 +52,7 @@
       imports = [
         ./formatter/flake-module.nix
         ./terraform/flake-module.nix
+        ./non-critical-infra/flake-module.nix
       ];
     };
-
 }
---
+++ non-critical-infra/flake-module.nix
@@ -0,0 +1,43 @@
+{ inputs, lib, ... }:
+{
+  flake =
+    let
+      importConfig =
+        path:
+        (lib.mapAttrs (name: _value: import (path + "/${name}/default.nix")) (
+          lib.filterAttrs (_: v: v == "directory") (builtins.readDir path)
+        ));
+    in
+    {
+      nixosConfigurations = builtins.mapAttrs (
+        _name: value:
+        inputs.nixpkgs.lib.nixosSystem {
+          inherit lib;
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            value
+            inputs.disko.nixosModules.disko
+            inputs.first-time-contribution-tagger.nixosModule
+            inputs.sops-nix.nixosModules.sops
+          ];
+          extraModules = [ inputs.colmena.nixosModules.deploymentOptions ];
+
+        }
+      ) (importConfig ./hosts);
+    };
+
+  perSystem =
+    { pkgs, inputs', ... }:
+    {
+      devShells.non-critical-infra = pkgs.mkShellNoCC {
+        packages = [
+          inputs'.colmena.packages.colmena
+          pkgs.sops
+          pkgs.ssh-to-age
+        ];
+      };
+    };
+}
