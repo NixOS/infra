20a75dab0c8cd26744558bd9cb70cc21f4b73afb
Jörg Thalheim <joerg@thalheim.io>
2024-08-18 17:01:10 +0200

    workaround space usage in CI

--- checks/flake-module.nix
+++ checks/flake-module.nix
@@ -5,9 +5,11 @@
     {
       checks =
         let
-          nixosMachines = lib.mapAttrs' (
-            name: config: lib.nameValuePair "nixos-${name}" config.config.system.build.toplevel
-          ) ((lib.filterAttrs (_: config: config.pkgs.system == system)) self.nixosConfigurations);
+          # TODO: our CI doesn't have a enough space for these just now
+          #nixosMachines = lib.mapAttrs' (
+          #  name: config: lib.nameValuePair "nixos-${name}" config.config.system.build.toplevel
+          #) ((lib.filterAttrs (_: config: config.pkgs.system == system)) self.nixosConfigurations);
+          nixosMachines = { };
 
           packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
           devShells = lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self'.devShells;
