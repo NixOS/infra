e0814c9fd69170106d71cd15e8d1d7512a593c84
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-11-09 12:26:08 -0600

    Customize bounce template for mailserver
    The default template tells you to "please send mail to postmaster"
    which isn't very actionable (what's the postmaster?). This directs
    people to a place where they can actually get help.

--- non-critical-infra/modules/mailserver/default.nix
+++ non-critical-infra/modules/mailserver/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [ ./mailing-lists.nix ];
@@ -34,4 +34,52 @@
       loginAccount.encryptedHashedPassword = ../../secrets/test-sender-email-login.umbriel;
     };
   };
+
+  services.postfix.config.bounce_template_file = "${pkgs.writeText "bounce-template.cf" ''
+    failure_template = <<EOF
+    Charset: us-ascii
+    From: MAILER-DAEMON (Mail Delivery System)
+    Subject: Undelivered Mail Returned to Sender
+    Postmaster-Subject: Postmaster Copy: Undelivered Mail
+
+    This is the mail system at host $myhostname.
+
+    I'm sorry to have to inform you that your message could not
+    be delivered to one or more recipients. It's attached below.
+
+    For further assistance, please file an issue at
+    https://github.com/NixOS/infra/issues/new. Please anonymize any personal
+    email addresses in your report.
+
+    If you do so, please include this problem report. You can
+    delete your own text from the attached returned message.
+
+                  The mail system
+    EOF
+
+    delay_template = <<EOF
+    Charset: us-ascii
+    From: MAILER-DAEMON (Mail Delivery System)
+    Subject: Delayed Mail (still being retried)
+    Postmaster-Subject: Postmaster Warning: Delayed Mail
+
+    This is the mail system at host $myhostname.
+
+    ####################################################################
+    # THIS IS A WARNING ONLY.  YOU DO NOT NEED TO RESEND YOUR MESSAGE. #
+    ####################################################################
+
+    Your message could not be delivered for more than $delay_warning_time_hours hour(s).
+    It will be retried until it is $maximal_queue_lifetime_days day(s) old.
+
+    For further assistance, please file an issue at
+    https://github.com/NixOS/infra/issues/new. Please anonymize any personal
+    email addresses in your report.
+
+    If you do so, please include this problem report. You can
+    delete your own text from the attached returned message.
+
+                       The mail system
+    EOF
+  ''}";
 }
