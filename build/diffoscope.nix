6b64bd57bd66a6f3b21e6f0f66b7daa80e3ef12e
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-12-11 02:43:37 +0100

    build: fix diffoscope wrapper indent

--- build/diffoscope.nix
+++ build/diffoscope.nix
@@ -1,17 +1,18 @@
-{ pkgs, ... }:
-
-with pkgs.lib;
+{
+  pkgs,
+  ...
+}:
 
 let
 
   diffoscopeWrapper = pkgs.writeScript "diffoscope-wrapper" ''
-            #! ${pkgs.stdenv.shell}
-    	exec >&2
-    	echo ""
-            echo "non-determinism detected in $2; diff with previous round follows:"
-            echo ""
-            time ${pkgs.utillinux}/bin/runuser -u diffoscope -- ${pkgs.diffoscope}/bin/diffoscope "$1" "$2"
-            exit 0
+    #! ${pkgs.stdenv.shell}
+    exec >&2
+    echo ""
+    echo "non-determinism detected in $2; diff with previous round follows:"
+    echo ""
+    time ${pkgs.utillinux}/bin/runuser -u diffoscope -- ${pkgs.diffoscope}/bin/diffoscope "$1" "$2"
+    exit 0
   '';
 
 in
