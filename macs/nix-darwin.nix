a00f52f8cf0561ba3b0c5826e8cb5760781052f0
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-12-19 15:51:08 +0100

    Revert "macs: allow gcing up to 25% of the store"
    This reverts commit eefbd1a08994b7232caa44399ce40e19a3d216fe.

--- macs/nix-darwin.nix
+++ macs/nix-darwin.nix
@@ -63,14 +63,13 @@ in
       automatic = true;
       user = "root";
       interval = {
-        # hourly at the 15th minute
         Minute = 15;
       };
       options =
         let
-          freePercentage = 0.25; # 25% free
+          gbFree = 100;
         in
-        "--max-freed $(df -k /nix/store | awk 'NR==2 {printf \"%.0f\n\", $2 * 1024 * ${toString freePercentage}}')";
+        "--max-freed $((${toString gbFree} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | awk '{ print $4 }')))";
     };
   };
 
