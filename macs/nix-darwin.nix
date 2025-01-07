f34304ddc32ee8faafe7917fc34171b6ff1b2054
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-07 01:40:55 +0100

    macs: workaround home dir validation issue for node-exporter

--- macs/nix-darwin.nix
+++ macs/nix-darwin.nix
@@ -85,6 +85,9 @@ in
 
   services.prometheus.exporters.node.enable = true;
 
+  # https://github.com/LnL7/nix-darwin/issues/1256
+  users.users._prometheus-node-exporter.home = lib.mkForce "/private/var/lib/prometheus-node-exporter";
+
   launchd.daemons.rosetta2-gc = {
     script = ''
       date
