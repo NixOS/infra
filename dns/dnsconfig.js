3f56afd494f0e46e3e7b3e6b3b0f4e6c878efe26
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-04-17 02:59:35 +0200

    dns: migrate nixcon.org

--- dns/dnsconfig.js
+++ dns/dnsconfig.js
@@ -5,7 +5,8 @@ DEFAULTS(
 var REG_NONE = NewRegistrar("none");
 var DSP_GANDI = NewDnsProvider("gandi");
 
-require("ofborg.org.js");
+require("nixcon.org.js");
 require("nix.dev.js");
 require("nixos.org.js");
+require("ofborg.org.js");
 
---
+++ dns/nixcon.org.js
@@ -0,0 +1,43 @@
+D("nixcon.org",
+	REG_NONE,
+	DnsProvider(DSP_GANDI),
+
+	MX("@", 10, "umbriel"),
+	SPF_BUILDER({
+		label: "@",
+		parts: [
+			"v=spf1",
+			"a:umbriel.nixos.org",
+			"-all"
+		]
+	}),
+	// Matching private key in `non-critical-infra/secrets/nixcon.org.mail.key.umbriel`
+	TXT("mail._domainkey", "p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC1wQ2uPZfdlGmjDDxeNVet7IEFxS55TpWuqQWNKmd4fX8HcKKw7kVHXU5+gjT37wMUI27ZZnIobYhumnl+BLiXZqbuzAt7s3dbJU2de2ZWxOqcDRbK6m2A3AwIAiMzzRUjx14EWgnw55KRi2enpLyS0pKGdvSquHnxaySkAF8YIwIDAQAB"),
+	DMARC_BUILDER({
+		policy: "none",
+	}),
+
+	// Websites
+	TXT("_github-pages-challenge-nixcon", "6608e513e09036ab8cadb7ca4eb71b"),
+
+	ALIAS("@", "nixcon.github.io."),
+	CNAME("www", "nixcon.github.io."),
+
+	CNAME("2015", "nixcon.github.io."),
+	CNAME("2016", "nixcon.github.io."),
+	CNAME("2017", "nixcon.github.io."),
+	CNAME("2018", "nixcon.github.io."),
+	CNAME("2019", "nixcon.github.io."),
+	CNAME("2020", "nixcon.github.io."),
+	CNAME("2022", "nixcon.github.io."),
+	CNAME("2023", "nixcon.github.io."),
+	CNAME("2024-na", "nixcon.github.io."),
+	CNAME("2024", "nixcon.github.io."),
+
+	// Scheduling
+	CNAME("cfp", "pretalx.com."),
+	CNAME("talks", "pretalx.com."),
+
+	// Ticketing
+	CNAME("tickets", "nixcon.cname.pretix.eu.")
+);
