d18f73c49a8505c9634612f797986d2b6e016c80
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-05-05 15:46:55 +0200

    flake.nix: reduce number of nixpkgs instances we track

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nixos org infra";
+  description = "NixOS.org infra";
 
   nixConfig.extra-substituters = [ "https://nixos-infra-dev.cachix.org" ];
   nixConfig.extra-trusted-public-keys = [
@@ -7,24 +7,34 @@
   ];
 
   inputs = {
-    agenix.url = "github:ryantm/agenix";
-    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    hydra.url = "github:NixOS/hydra";
-    hydra.inputs.nixpkgs.follows = "nixpkgs";
+    hydra = {
+      url = "github:NixOS/hydra";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    nixos-channel-scripts.url = "github:NixOS/nixos-channel-scripts";
-    nixos-channel-scripts.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-channel-scripts = {
+      url = "github:NixOS/nixos-channel-scripts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    rfc39.url = "github:NixOS/rfc39";
-    rfc39.inputs.nixpkgs.follows = "nixpkgs";
+    rfc39 = {
+      url = "github:NixOS/rfc39";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11-small";
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
 
     darwin = {
       url = "github:LnL7/nix-darwin/nix-darwin-24.11";
@@ -32,7 +42,11 @@
     };
 
     flake-utils.url = "github:numtide/flake-utils";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
     colmena = {
       url = "github:zhaofengli/colmena";
@@ -42,19 +56,30 @@
         stable.follows = "nixpkgs";
       };
     };
+
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     srvos = {
       url = "github:numtide/srvos";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    simple-nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
+
+    simple-nixos-mailserver = {
+      url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-unstable";
+        nixpkgs-24_11.follows = "nixpkgs";
+      };
+    };
+
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     first-time-contribution-tagger = {
       url = "github:Janik-Haag/first-time-contribution-tagger";
       inputs = {
