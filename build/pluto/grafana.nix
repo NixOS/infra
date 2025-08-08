c025f129be25f350e3f154aed5f9f1feae258b46
Dionysis Grigoropoulos <dgrig@erethon.com>
2025-08-08 13:22:48 +0300

    pluto: Allow users to access Explore in Grafana

--- build/pluto/grafana.nix
+++ build/pluto/grafana.nix
@@ -5,7 +5,10 @@
     enable = true;
     settings = {
       "auth.anonymous".enabled = true;
-      users.allow_sign_up = true;
+      users = {
+        allow_sign_up = true;
+        viewers_can_edit = true;
+      };
       server = {
         domain = "grafana.nixos.org";
         root_url = "https://grafana.nixos.org";
