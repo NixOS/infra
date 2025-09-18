c21bc89ef5fefa200913c8581bef94217ebe78dd
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-09-18 14:02:50 -0500

    Set up backups for umbriel
    This does not actually back up any files/folders yet, it just sets up
    the infrastructure for doing so. This is necessary because umbriel is
    now stateful (now that we use it for freescout).

--- non-critical-infra/hosts/umbriel/default.nix
+++ non-critical-infra/hosts/umbriel/default.nix
@@ -1,4 +1,9 @@
-{ inputs, lib, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 
 {
   imports = [
@@ -6,6 +11,7 @@
     inputs.srvos.nixosModules.server
     inputs.srvos.nixosModules.hardware-hetzner-cloud-arm
     ../../modules/common.nix
+    ../../modules/backup.nix
     ../../modules/prometheus/node-exporter.nix
     ../../modules/mailserver
   ];
@@ -42,6 +48,40 @@
       }
     ];
     linkConfig.RequiredForOnline = "routable";
+  };
+
+  # How to generate:
+  #
+  #   $ cd non-critical-infra
+  #   $ SECRET_PATH=secrets/freescout-app-key.umbriel
+  #   $ ssh-keygen -t ed25519 -f "$SECRET_PATH" -P "" -C root@umbriel
+  #   $ sops encrypt --in-place "$SECRET_PATH"
+  #   $ rm "$SECRET_PATH".pub
+  sops.secrets.storagebox-ssh-key = {
+    sopsFile = ../../secrets/storagebox-ssh-key.umbriel;
+    format = "binary";
+    path = "/var/keys/storagebox-ssh-key";
+    mode = "0600";
+    owner = "root";
+    group = "root";
+  };
+
+  sops.secrets.backup-secret = {
+    sopsFile = ../../secrets/backup-secret.umbriel; # <<< TODO: how to generate >>>
+    format = "binary";
+    path = "/var/keys/borg-secret";
+    mode = "0600";
+    owner = "root";
+    group = "root";
+  };
+
+  services.backup = {
+    user = "u391032-sub3";
+    host = "u391032-sub3.your-storagebox.de";
+    hostPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIICf9svRenC/PLKIL9nk6K/pxQgoiFC41wTNvoIncOxs";
+    port = 23;
+    sshKey = config.sops.secrets.storagebox-ssh-key.path;
+    secretPath = config.sops.secrets.backup-secret.path;
   };
 
   system.stateVersion = "23.05";
