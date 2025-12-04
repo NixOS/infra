f7d084e5778bdb9e24ba7f60cabac7e414d778a0
Simon Hauser <simon.hauser@helsinki-systems.de>
2025-12-04 11:39:46 +0100

    hydra-queue-runner: 66d8c5d0 -> ede19bdf

--- non-critical-infra/packages/hydra-queue-runner/default.nix
+++ non-critical-infra/packages/hydra-queue-runner/default.nix
@@ -18,10 +18,10 @@ let
   src = fetchFromGitHub {
     owner = "helsinki-systems";
     repo = "hydra-queue-runner";
-    rev = "66d8c5d094987f6f54ec81b488812b999358267b";
-    hash = "sha256-MwmnMExE2xDtZZFUXVec1xThvGx2GJUdFEVVOMtlF3Q=";
+    rev = "ede19bdf00e737f35f9bbef8df79dd5ea5be7642";
+    hash = "sha256-tQTSjzh32jaby+0O6u+16zwZvUolYaWR7ENlR4+S+dI=";
   };
-  cargoHash = "sha256-jfM+1fa0LhIP0aB+sbEsRY1ps2cM2PLqlno/W+Lc1lQ=";
+  cargoHash = "sha256-MzyIdXgYQXz4hI7qUHBWvU036nsc2/e62cF18FnOjYw=";
   nativeBuildInputs = [
     pkg-config
     protobuf
