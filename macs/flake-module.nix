953ad2f50630f9162c81f46015d87624f2601aae
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-12-22 22:17:13 +0100

    macs: create individual darwin configurations for each machine
    Now with secrets being tied to each machine we need a dedicated profile
    for each machine.

--- macs/flake-module.nix
+++ macs/flake-module.nix
@@ -2,19 +2,30 @@
 {
   flake.darwinConfigurations =
     let
-      mac =
-        system: entrypoint:
+      mkNixDarwin =
+        localHostName: entrypoint:
         inputs.darwin.lib.darwinSystem {
-          inherit system;
+          system = "aarch64-darwin";
 
           modules = [
+            {
+              networking = { inherit localHostName; };
+            }
             ./common.nix
             entrypoint
           ];
         };
     in
     {
-      m1 = mac "aarch64-darwin" ./profiles/m1.nix;
-      m2-large = mac "aarch64-darwin" ./profiles/m2.large.nix;
+      # M1 8C, 16G, 256G (Hetzner)
+      enormous-catfish = mkNixDarwin "enormous-catfish" ./profiles/m1.nix;
+      growing-jennet = mkNixDarwin "growing-jennet" ./profiles/m1.nix;
+      intense-heron = mkNixDarwin "intense-heron" ./profiles/m1.nix;
+      maximum-snail = mkNixDarwin "maximum-snail" ./profiles/m1.nix;
+      sweeping-filly = mkNixDarwin "sweeping-filly" ./profiles/m1.nix;
+
+      # M2 8C, 24G, 1TB (Oakhost)
+      eager-heisenberg = mkNixDarwin "eager-heisenberg" ./profiles/m2.large.nix;
+      kind-lumiere = mkNixDarwin "kind-lumiere" ./profiles/m2.large.nix;
     };
 }
