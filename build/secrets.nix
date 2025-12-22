154d3ed4b8da1b3d8346fe81584c94387286a0fb
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-12-22 16:55:44 +0100

    builders: enable hydra-queue-runner

--- build/secrets.nix
+++ build/secrets.nix
@@ -17,6 +17,24 @@ let
     rfc39-record-push = [ machines.pluto ];
     tarball-mirror-aws-credentials = [ machines.pluto ];
     zrepl-ssh-key = [ machines.titan ];
+
+    # builders/
+    elated-minsky-queue-runner-token = with machines; [
+      mimas
+      elated-minsky
+    ];
+    goofy-hopcroft-queue-runner-token = with machines; [
+      mimas
+      goofy-hopcroft
+    ];
+    hopeful-rivest-queue-runner-token = with machines; [
+      mimas
+      hopeful-rivest
+    ];
+    sleepy-brown-queue-runner-token = with machines; [
+      mimas
+      sleepy-brown
+    ];
   };
 in
 builtins.listToAttrs (
