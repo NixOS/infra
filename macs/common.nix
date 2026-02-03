8b27622bca81efc9b57cf1ff5b61ae34b3373ef3
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-02-03 01:43:57 +0100

    macs: gc twice per hour for up to 100 GB
    We see too many alerts about disk fullness and jobs stuck sending inputs
    because there is no space remaining, so we now gc harder.

--- macs/common.nix
+++ macs/common.nix
@@ -54,12 +54,16 @@ in
     };
     gc = {
       automatic = true;
-      interval = {
-        # hourly at the 15th minute
-        Minute = 15;
-      };
-      # ensure up to 125G free space every hour
-      options = "--max-freed $(df -k /nix/store | awk 'NR==2 {available=$4; required=125*1024*1024; to_free=required-available; printf \"%.0d\", to_free*1024}')";
+      interval = [
+        {
+          Minute = 15;
+        }
+        {
+          Minute = 45;
+        }
+      ];
+      # ensure up to 100G free space every half hour
+      options = "--max-freed $(df -k /nix/store | awk 'NR==2 {available=$4; required=100*1024*1024; to_free=required-available; printf \"%.0d\", to_free*1024}')";
     };
   };
 
