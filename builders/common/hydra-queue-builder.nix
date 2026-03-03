b8f894b0f597e6dbd5da97ab16576ce3dd4365ef
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-03-03 01:29:11 +0100

    macs, builders: properly guard queue-builder secret deployment

--- builders/common/hydra-queue-builder.nix
+++ builders/common/hydra-queue-builder.nix
@@ -1,6 +1,7 @@
 {
   config,
   inputs,
+  lib,
   ...
 }:
 
@@ -9,14 +10,16 @@
     inputs.hydra-queue-runner.nixosModules.queue-builder
   ];
 
-  age.secrets."queue-runner-token" = {
-    file = ../../build/secrets/${config.networking.hostName}-queue-runner-token.age;
-    owner = "hydra-queue-builder";
-  };
+  config = lib.mkIf false {
+    age.secrets."queue-runner-token" = {
+      file = ../../build/secrets/${config.networking.hostName}-queue-runner-token.age;
+      owner = "hydra-queue-builder";
+    };
 
-  services.queue-builder-dev = {
-    enable = false;
-    queueRunnerAddr = "https://queue-runner.hydra.nixos.org";
-    authorizationFile = config.age.secrets."queue-runner-token".path;
+    services.queue-builder-dev = {
+      enable = true;
+      queueRunnerAddr = "https://queue-runner.hydra.nixos.org";
+      authorizationFile = config.age.secrets."queue-runner-token".path;
+    };
   };
 }
--- macs/hydra-queue-builder.nix
+++ macs/hydra-queue-builder.nix
@@ -11,15 +11,17 @@
     inputs.hydra-queue-runner.darwinModules.queue-builder
   ];
 
-  age.secrets."queue-runner-token" = {
-    file = ../build/secrets/${config.networking.localHostName}-queue-runner-token.age;
-    owner = "hydra-queue-builder";
-  };
+  config = lib.mkIf false {
+    age.secrets."queue-runner-token" = {
+      file = ../build/secrets/${config.networking.localHostName}-queue-runner-token.age;
+      owner = "hydra-queue-builder";
+    };
 
-  services.queue-builder-dev = {
-    enable = false;
-    queueRunnerAddr = "https://queue-runner.hydra.nixos.org";
-    authorizationFile = config.age.secrets."queue-runner-token".path;
-    maxJobs = if lib.elem "big-parallel" (config.nix.settings.system-features or [ ]) then 2 else 4;
+    services.queue-builder-dev = {
+      enable = true;
+      queueRunnerAddr = "https://queue-runner.hydra.nixos.org";
+      authorizationFile = config.age.secrets."queue-runner-token".path;
+      maxJobs = if lib.elem "big-parallel" (config.nix.settings.system-features or [ ]) then 2 else 4;
+    };
   };
 }
