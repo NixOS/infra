01cf700adcaed8a1f10d9ee574361e983e062a26
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-06-18 19:59:16 +0200

    macs: reboot every sunday between 0:00 and 3:00
    Imported from https://github.com/nix-community/infra/blob/master/modules/darwin/common/reboot.nix?rgh-link-date=2026-05-19T05%3A34%3A54.000Z
    
    Closes: #1042

--- macs/production.nix
+++ macs/production.nix
@@ -3,6 +3,7 @@
     ./common/hydra-queue-builder.nix
     ./common/nix.nix
     ./common/node-exporter.nix
+    ./common/reboot.nix
     ./common/shells.nix
     ./common/spotlight.nix
     ./common/ssh.nix
