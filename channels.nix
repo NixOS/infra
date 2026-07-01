ef0aa6d84b63a0bfc184f1c96a636a9cb97386e0
Yaya <github@uwu.is>
2026-07-01 11:49:59 +0200

    channels: Mark 25.11 as unmaintained

--- channels.nix
+++ channels.nix
@@ -58,17 +58,17 @@ rec {
     "nixos-25.11" = {
       job = "nixos/release-25.11/tested";
       variant = "primary";
-      status = "deprecated";
+      status = "unmaintained";
     };
     "nixos-25.11-small" = {
       job = "nixos/release-25.11-small/tested";
       variant = "small";
-      status = "deprecated";
+      status = "unmaintained";
     };
     "nixpkgs-25.11-darwin" = {
       job = "nixpkgs/nixpkgs-25.11-darwin/darwin-tested";
       variant = "darwin";
-      status = "deprecated";
+      status = "unmaintained";
     };
 
     "nixos-25.05" = {
