521b79f52878b1142848b9f484839eb71dac36fa
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-08 23:53:30 +0200

    hydra-proxy: ban AS62610 (ZEN-DPS)
    In 3 days we saw the following number of requests and IP distribution:
    
    AS62610 121809 (121792 IPs)

--- build/hydra-proxy.nix
+++ build/hydra-proxy.nix
@@ -48,6 +48,7 @@
             37963 # ALIBABA-CN-NET
             45102 # ALIBABA-CN-NET
             45899 # VNPT-AS-VN
+            62610 # ZEN-DPS
             132203 # TENCENT-NET-AP-CN
           ];
         };
