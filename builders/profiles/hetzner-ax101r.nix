b03f5e332d7d296c742b23696a9c371a91c37747
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-12 02:25:09 +0200

    builders: drop tmpfs build dir on ax101r
    Sorry for taking so long, but it's not worth the hassle for so little
    perf gain.

--- builders/profiles/hetzner-ax101r.nix
+++ builders/profiles/hetzner-ax101r.nix
@@ -1,6 +1,4 @@
 {
-  config,
-  lib,
   ...
 }:
 
@@ -13,25 +11,6 @@
   boot.supportedFilesystems.zfs = true;
   boot.zfs.forceImportRoot = false;
   networking.hostId = "91312b0a";
-
-  fileSystems."/nix/var/nix/builds" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = [
-      "huge=within_size"
-      "mode=0700"
-      "nosuid"
-      "nodev"
-    ]
-    # 128G tmpfs, 128G RAM (+zram swap) for standard builders
-    # 160GB tmpfs, 96 GB RAM (+zram swap) for big-parallel builders
-    ++ (
-      if lib.elem "big-parallel" config.services.hydra-queue-builder-dev.supportedFeatures then
-        [ "size=160G" ]
-      else
-        [ "size=128G" ]
-    );
-  };
 
   boot.initrd.availableKernelModules = [
     "nvme"
