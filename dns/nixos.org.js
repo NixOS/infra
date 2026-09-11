2ac7d3f046fc055061458b13f2485da4ab1ef2e5
Jörg Thalheim <joerg@thalheim.io>
2026-09-11 13:00:33 +0200

    fastly: drop test.wiki.nixos.org
    wiki.nixos.org itself is served by Fastly now.

--- dns/nixos.org.js
+++ dns/nixos.org.js
@@ -208,10 +208,6 @@ D("nixos.org",
 	}),
 	TXT("mail._domainkey.wiki", "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDa+KjIljYr3q5MWWK7sEYzjR8OcA32zBh9BCPo6/HlY1q2ODTYsmE/FDZWpYMzM5z+ddnuGYdXia322XnZaNpZNoq1TbGYuQ5DsgAEK09CGoLuzONg3PSXTrkG7E2Sd6wstwHGJ5FHxSLKtNoWkknt9F5XAFZgXapO0w54p+BWvwIDAQAB"),
 
-	// test.wiki subdomain with Fastly
-	CNAME("test.wiki", "dualstack.n.sni.global.fastly.net."),
-	CNAME("_acme-challenge.test.wiki", "zsz0meyel8hxoy9dtb.fastly-validations.com."),
-
 	// github org/domain binding
 	TXT("_github-challenge-nixos", "9e10a04a4b"),
 
