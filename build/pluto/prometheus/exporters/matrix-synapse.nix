576cdd5f9a83e63d5da26cb910c134f74fc62e1e
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-09-23 21:25:08 +0200

    prometheus: fix synapse labels for grafana dashboard

--- build/pluto/prometheus/exporters/matrix-synapse.nix
+++ build/pluto/prometheus/exporters/matrix-synapse.nix
@@ -1,29 +1,57 @@
 {
+  lib,
+  ...
+}:
+
+{
   services.prometheus.scrapeConfigs = [
     {
       job_name = "matrix_synapse";
       scheme = "https";
 
-      static_configs =
+      static_configs = lib.flatten (
         map
-          (index: {
-            targets = [ "matrix.nixos.org:443" ];
-            labels = {
-              inherit index;
-              __metrics_path__ = "/metrics/${index}";
-            };
-          })
+          (
+            {
+              job,
+              indices,
+            }:
+            map (index: {
+              targets = [ "matrix.nixos.org:443" ];
+              labels = {
+                job = "synapse${if job != "main" then "-${job}" else ""}";
+                __metrics_path__ = "/metrics/${job}${toString index}";
+              }
+              // lib.optionalAttrs (index != null) {
+                index = toString index;
+              };
+            }) indices
+          )
           [
-            "main"
-            "client1"
-            "client2"
-            "client3"
-            "client4"
-            "federation_sender1"
-            "federation_sender2"
-            "federation_sender3"
-            "federation_sender4"
-          ];
+            {
+              job = "main";
+              indices = [ null ];
+            }
+            {
+              job = "client";
+              indices = [
+                1
+                2
+                3
+                4
+              ];
+            }
+            {
+              job = "federation_sender";
+              indices = [
+                1
+                2
+                3
+                4
+              ];
+            }
+          ]
+      );
     }
   ];
 }
